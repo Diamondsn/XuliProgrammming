8c3cb00c1e0b746053033763dc5ddf0d7748758b
xl <1756625653@qq.com>
2018-09-11 23:04:13 +0800

    update

--- 20180911牛客网剑指offer刷题/二叉树中和为某一值的路径.cpp
+++ 20180911牛客网剑指offer刷题/二叉树中和为某一值的路径.cpp
@@ -26,8 +26,61 @@ struct TreeNode {
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-vector<vector<int> > FindPath(TreeNode* root, int expectNumber) {
+//快速排序
+void kuaipai(vector<vector<int> >& vec)
+{
 
+}
+//归并排序
+void guibing(vector<vector<int> >& vec1, vector<vector<int> >& vec2, vector<vector<int> >& result)
+{
+	kuaipai(vec1);
+	kuaipai(vec2);
+
+}
+
+vector<vector<int> > FindPath(TreeNode* root, int expectNumber) {
+	vector<vector<int> >result;
+	vector<int> temp;
+	if (root == NULL)
+		return result;
+
+	vector<vector<int> >left;
+	vector<vector<int> >right;
+
+	if (root->left)
+		left=FindPath(root->left, expectNumber - root->val);
+	if (root->right)
+		right=FindPath(root->right, expectNumber - root->val);
+
+	for (size_t i = 0; i < left.size(); ++i)
+	{
+		if (left[i].size() < 1)
+			continue;
+
+		left[i].push_back(left[i][left[i].size() - 1]);
+		for (size_t j = left[i].size()-1; j >=1; --j)
+		{
+			left[i][j] = left[i][j - 1];
+		}
+		left[i][0] = root->val;
+	}
+	for (size_t i = 0; i < right.size(); ++i)
+	{
+		if (right[i].size() < 1)
+			continue;
+
+		right[i].push_back(right[i][right[i].size() - 1]);
+		for (size_t j = right[i].size() - 1; j >= 1; --j)
+		{
+			right[i][j] = right[i][j - 1];
+		}
+		right[i][0] = root->val;
+	}
+
+	guibing(left, right, result);
+
+	return result;
 }
 
 void Qianxubianli(TreeNode* pNode)
@@ -62,21 +115,36 @@ void Houxubianli(TreeNode* pNode)
 
 int main() {
 	//test
-	TreeNode* t = new TreeNode(3);
-	TreeNode* t_39 = new TreeNode(9);
-	TreeNode* t_20 = new TreeNode(20);
-	TreeNode* t_15 = new TreeNode(15);
-	TreeNode* t_7 = new TreeNode(7);
-
-	t->left = t_39;
-	t->right = t_20;
-	t_20->left = t_15;
-	t_20->right = t_7;
-
-	vector<int> vec = PrintFromTopToBottom(t);
-	for (int i = 0; i < vec.size(); ++i)
+	TreeNode* t = new TreeNode(5);
+	TreeNode* t_1=new TreeNode(1);
+	TreeNode* t_2 = new TreeNode(2);
+	t->left = t_1;
+	t->right = t_2;
+	TreeNode* t_3 = new TreeNode(3);
+	TreeNode* t_4 = new TreeNode(4);
+	t_1->left = t_3;
+	t_1->right = t_4;
+	TreeNode* t_5 = new TreeNode(5);
+	TreeNode* t_6 = new TreeNode(6);
+	t_2->left = t_5;
+	t_2->right = t_6;
+	TreeNode* t_3_2 = new TreeNode(3);
+	TreeNode* t_2_2 = new TreeNode(2);
+	t_3->left = t_3_2;
+	t_3->right = t_2_2;
+	TreeNode* t_2_3 = new TreeNode(2);
+	TreeNode* t_1_2 = new TreeNode(1);
+	t_4->left = t_2_3;
+	t_4->right = t_1_2;
+	vector < vector<int> > result= FindPath(t,12);
+	for (size_t i = 0; i < result.size(); ++i)
 	{
-		cout << vec[i] << endl;
+		cout << "第" << i << "条路径为" << endl;
+		for (size_t j = 0; j < result[i].size(); ++j)
+		{
+			cout << result[i][j] << "-";
+		}
+		cout << endl;
 	}
 	//test end
 
