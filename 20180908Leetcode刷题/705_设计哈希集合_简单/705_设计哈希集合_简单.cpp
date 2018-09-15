8688eaf21a72d0a497f459f94bd88e00b8f89a94
xl <1756625653@qq.com>
2018-09-15 16:02:22 +0800

    update

---
+++ 20180908Leetcode刷题/705_设计哈希集合_简单/705_设计哈希集合_简单.cpp
@@ -0,0 +1,78 @@
+﻿#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+//不使用任何内建的哈希表库设计一个哈希集合
+//
+//具体地说，你的设计应该包含以下的功能
+//
+//add(value)：向哈希集合中插入一个值。
+//contains(value) ：返回哈希集合中是否存在这个值。
+//remove(value)：将给定值从哈希集合中删除。如果哈希集合中没有这个值，什么也不做。
+//
+//示例 :
+//MyHashSet hashSet = new MyHashSet();
+//hashSet.add(1);
+//hashSet.add(2);
+//hashSet.contains(1);    // 返回 true
+//hashSet.contains(3);    // 返回 false (未找到)
+//hashSet.add(2);
+//hashSet.contains(2);    // 返回 true
+//hashSet.remove(2);
+//hashSet.contains(2);    // 返回  false (已经被删除)
+//
+//注意：
+//所有的值都在[1, 1000000]的范围内。
+//操作的总数目在[1, 10000]范围内。
+//不要使用内建的哈希集合库。
+
+//使用数组存储bool值减轻存储负担
+class MyHashSet {
+public:
+	/** Initialize your data structure here. */
+	MyHashSet() {
+		hashset = vector<bool>(1000001, false);
+	}
+
+	void add(int key) {
+		hashset[key] = true;
+	}
+
+	void remove(int key) {
+		hashset[key] = false;
+	}
+
+	/** Returns true if this set did not already contain the specified element */
+	bool contains(int key) {
+		return hashset[key];
+	}
+private:
+	vector<bool>hashset;
+};
+
+/**
+* Your MyHashSet object will be instantiated and called as such:
+* MyHashSet obj = new MyHashSet();
+* obj.add(key);
+* obj.remove(key);
+* bool param_3 = obj.contains(key);
+*/
+
+int main() {
+	//test1
+	MyHashSet hashSet = MyHashSet();
+	hashSet.add(1);
+	hashSet.add(2);
+	bool a=hashSet.contains(1);    // 返回 true
+	bool b=hashSet.contains(3);    // 返回 false (未找到)
+	hashSet.add(2);
+	bool c=hashSet.contains(2);    // 返回 true
+	hashSet.remove(2);
+	bool d=hashSet.contains(2);    // 返回  false (已经被删除)
+	cout << a << endl << b << endl << c << endl << d << endl;
+	//test end
+	system("pause");
+	return 0;
+}
---
+++ 20180908Leetcode刷题/762_二进制表示中质数个计算置位_简单/762_二进制表示中质数个计算置位_简单.cpp
@@ -0,0 +1,82 @@
+﻿#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+//给定两个整数 L 和 R ，找到闭区间 [L, R] 范围内，计算置位位数为质数的整数个数。
+//（注意，计算置位代表二进制表示中1的个数。例如 21 的二进制表示 10101 有 3 个计算置位。
+//还有，1 不是质数。）
+//
+//示例 1:
+//输入: L = 6, R = 10
+//输出: 4
+//解释:
+//6 -> 110 (2 个计算置位，2 是质数)
+//7 -> 111 (3 个计算置位，3 是质数)
+//9 -> 1001 (2 个计算置位，2 是质数)
+//10-> 1010 (2 个计算置位，2 是质数)
+//
+//示例 2:
+//输入: L = 10, R = 15
+//输出: 5
+//解释:
+//10 -> 1010 (2 个计算置位, 2 是质数)
+//11 -> 1011 (3 个计算置位, 3 是质数)
+//12 -> 1100 (2 个计算置位, 2 是质数)
+//13 -> 1101 (3 个计算置位, 3 是质数)
+//14 -> 1110 (3 个计算置位, 3 是质数)
+//15 -> 1111 (4 个计算置位, 4 不是质数)
+//
+//注意:
+//L, R 是 L <= R 且在 [1, 10^6] 中的整数。
+//R - L 的最大值为 10000。
+
+int countPrimeSetBits(int L, int R) {
+	int result=0;
+	int zhiwei;
+	vector<int> zhishu;
+	zhishu.push_back(2);
+	//此处求得2到32中的所有质数
+	for (int i = 3; i < 33; ++i)
+	{
+		int flag =true;
+		for (int j = 0; j < zhishu.size(); ++j)
+		{
+			if (i%zhishu[j] == 0)
+			{
+				flag = false;
+				break;
+			}
+		}
+		if (flag)
+			zhishu.push_back(i);
+	}
+
+	for (int i = L; i <= R; ++i)
+	{
+		zhiwei = 0;
+		for (int j = 0; j < 32; ++j)
+		{
+			if ((i >> j) & 1 == 1)
+				zhiwei++;
+		}
+
+		if(find(zhishu.begin(),zhishu.end(),zhiwei)!=zhishu.end())
+			result++;
+	}
+	return result;
+}
+
+int main() {
+	//test1
+	int a = countPrimeSetBits(6, 10);
+
+	int b = countPrimeSetBits(10, 15);
+
+	cout <<a <<endl<< b << endl;
+	//test end
+
+	system("pause");
+	return 0;
+}
