15723400fb091cd88c28146b1faee33203c7a97c
Diamondsn <33226377+Diamondsn@users.noreply.github.com>
2018-09-17 14:40:13 +0800

    Create 9_回文数_简单.cpp

---
+++ 20180908Leetcode刷题/9/9_回文数_简单.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<string>
+#include<sstream>
+#include<stdint.h>
+using namespace std;
+
+//判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+//
+//示例 1:
+//输入: 121
+//输出: true
+//
+//示例 2:
+//输入: -121
+//输出: false
+//解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+//
+//示例 3:
+//输入: 10
+//输出: false
+//解释: 从右向左读, 为 01 。因此它不是一个回文数。
+//
+//进阶:
+//你能不将整数转为字符串来解决这个问题吗？
+
+bool isPalindrome(int x) {
+	if (x < 0 || (x%10==0 && x!=0))
+		return false;
+
+	int reverse=0;
+	while (x > reverse)
+	{
+		reverse = reverse * 10 + x % 10;
+		x /= 10;
+	}
+	return x == reverse || x == reverse / 10;
+}
+
+void main(){
+	//test1
+	bool a = isPalindrome(121);
+
+	//test2
+	bool b = isPalindrome(-121);
+
+	//test3
+	bool c = isPalindrome(10);
+
+	cout << a << endl << b << endl << c << endl;//1,0,0
+	//test end
+	system("pause");
+}
