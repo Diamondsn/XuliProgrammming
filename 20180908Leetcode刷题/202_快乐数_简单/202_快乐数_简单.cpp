24dfc84cfa5b1ee22087ce051ac9329a656cf7fc
xl <1756625653@qq.com>
2018-09-19 21:36:12 +0800

    update

---
+++ 20180908Leetcode刷题/202_快乐数_简单/202_快乐数_简单.cpp
@@ -0,0 +1,53 @@
+﻿#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+//编写一个算法来判断一个数是不是“快乐数”。
+//
+//一个“快乐数”定义为：对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和，然后重复这个过程直到这个数变为 1，也可能是无限循环但始终变不到 1。如果可以变为 1，那么这个数就是快乐数。
+//
+//示例: 
+//输入: 19
+//输出: true
+//解释: 
+//12 + 92 = 82
+//82 + 22 = 68
+//62 + 82 = 100
+//12 + 02 + 02 = 1
+
+bool isHappy(int n) {
+	if (n <= 0)
+		return false;
+
+	vector<int>vec;
+	int sum = 0;
+	while ((find(vec.begin(), vec.end(), sum) == vec.end()) && (sum != 1))//前面这部分需要括号
+	{
+		vec.push_back(n);
+		sum = 0;
+		while (n > 0)
+		{
+			int temp = n % 10;
+			n /= 10;
+			sum += temp*temp;
+		}
+		n = sum;
+	}
+	if (sum == 1)
+		return true;
+	else
+		return false;
+}
+
+void main() {
+	//test
+	/*bool a = isHappy(19);
+	cout << a << endl;*/
+	
+	//test1
+	bool b = isHappy(2);
+	cout << b << endl;
+	//test end
+	system("pause");
+}
--- 20180908Leetcode刷题/427_建立四叉树_简单/427_建立四叉树_简单.cpp
+++ 20180908Leetcode刷题/427_建立四叉树_简单/427_建立四叉树_简单.cpp
@@ -45,22 +45,124 @@ public:
 
 void IsLeaf(vector<vector<int>>& grid, int xMin, int xMax,int yMin,int yMax,bool& isLeaf,bool& val)
 {
-	if (xMax == xMin && yMax == yMin)
+	if (xMax- xMin==1 && yMax- yMin==1)
 	{
 		isLeaf = true;
-		val = grid[xMax][yMax];
+		val = grid[yMin][xMin];
 		return;
 	}
 
+	bool yu = true;//存储与值
+	bool huo = false;//存储或值
+	for (int i = yMin; i <yMax; ++i)
+	{
+		for (int j = xMin; j <xMax; ++j)
+		{
+			yu &= grid[i][j];
+			huo |= grid[i][j];
+		}
+	}
+	if (yu == true || huo == false)
+	{
+		isLeaf = true;
+		val = yu == true ? true:false ;
+		return;
+	}
+	isLeaf = false;
+	val = false;
 }
 
 Node* construct(vector<vector<int>>& grid) {
-        
+	int r = grid.size();
+	int c = grid[0].size();
+
+	if (r == 0 || c == 0)
+		return NULL;
+
+	bool leaf;
+	bool val;
+	IsLeaf(grid, 0, c, 0, r,leaf,val);
+	if (leaf == true)
+	{
+		Node* pNode = new Node(val, leaf, NULL, NULL, NULL, NULL);
+		return pNode;
+	}
+	else
+	{
+		vector<vector<int>> vectopLeft, vectopRight, vecbottomLeft, vecbottomRight;
+		vector<int> topLefttemp, topRighttemp, bottomLefttemp,bottomRighttemp;
+		for (int i = 0; i < r; ++i)
+		{
+			topLefttemp.clear();
+			topRighttemp.clear();
+			bottomLefttemp.clear();
+			bottomRighttemp.clear();
+			for (int j = 0; j <c; ++j)
+			{
+				if (i < r / 2 && j < c / 2)
+				{
+					topLefttemp.push_back(grid[i][j]);
+				}
+				else if (i < r / 2 && j >= c / 2)
+				{
+					topRighttemp.push_back(grid[i][j]);
+				}
+				else if (i >= r / 2 && j < c/2)
+				{
+					bottomLefttemp.push_back(grid[i][j]);
+				}
+				else if (i >= r / 2 && j >= c / 2)
+				{
+					bottomRighttemp.push_back(grid[i][j]);
+				}
+			}
+			if (topLefttemp.size() > 0)
+				vectopLeft.push_back(topLefttemp);
+			if (topRighttemp.size() > 0)
+				vectopRight.push_back(topRighttemp);
+			if (bottomLefttemp.size() > 0)
+				vecbottomLeft.push_back(bottomLefttemp);
+			if (bottomRighttemp.size() > 0)
+				vecbottomRight.push_back(bottomRighttemp);
+		}
+		Node* topLeft = construct(vectopLeft);
+		Node* topRight = construct(vectopRight);
+		Node* bottomLeft = construct(vecbottomLeft);
+		Node* bottomRight = construct(vecbottomRight);
+		Node* pNode = new Node(false, false, topLeft, topRight, bottomLeft, bottomRight);
+		return pNode;
+	}
 }
 
+//前序遍历四叉树
+void QianxuBianLiSiChashu(Node* pNode)
+{
+	if (pNode == NULL)
+		return;
+
+	cout << "值：" << pNode->val << "，是否是叶子节点" << pNode->isLeaf << endl;
+	if (pNode->topLeft)
+		QianxuBianLiSiChashu(pNode->topLeft);
+	if(pNode->topRight)
+		QianxuBianLiSiChashu(pNode->topRight);
+	if(pNode->bottomLeft)
+		QianxuBianLiSiChashu(pNode->bottomLeft);
+	if (pNode->bottomRight)
+		QianxuBianLiSiChashu(pNode->bottomRight);
+}
 void main() {
 	//test
-	
+	vector<vector<int>>grid = {
+		{1,1,1,1,0,0,0,0},
+		{1,1,1,1,0,0,0,0},
+		{1,1,1,1,1,1,1,1},
+		{1,1,1,1,1,1,1,1},
+		{1,1,1,1,0,0,0,0},
+	   { 1,1,1,1,0,0,0,0 },
+	   { 1,1,1,1,0,0,0,0 },
+	   { 1,1,1,1,0,0,0,0 } };
+	Node* pNode = construct(grid);
+	QianxuBianLiSiChashu(pNode);
 	//test end
 	system("pause");
 }
