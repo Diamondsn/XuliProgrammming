cb134dff406162713d12de76d7c01a8e03e911cf
xl <1756625653@qq.com>
2018-09-20 21:58:20 +0800

    update

---
+++ 20180920字节跳动/2数据库字符串唯一确定前缀.cpp
@@ -0,0 +1,94 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+#include<map>
+using namespace std;
+
+//题目描述:
+//数据库里的每条数据可以抽象成一个仅由小写字母构成的字符串。
+//现在相对于这些字符串建立前缀表示，产生的表示可以唯一确定一条
+//数据且产生前缀表示的总字符数最少。
+//输入数据确保没有一个字符串是另一个字符串的前缀。
+//例如:对于两个字符串:
+//abcdefg
+//abccfeg
+//我们乐意取这样的前缀作为每一个字符串的标识:
+//abcd
+//abcc
+//并可以方便的证明，这种前缀表示的方式总字符数最少。
+
+//输入描述
+//输入文件的第一行包含一个正整数N，表示字符串的数目。之后N行，每行
+//一个字符串，表示单词。
+
+//示例1
+//输入
+//5
+//bytedance
+//toutiaohao
+//toutiaoapp
+//iesaweme
+//iestiktok
+//输出
+//b
+//toutiaoh
+//toutiaoa
+//iesa
+//iest
+
+vector<string> test(vector<string>& vec)
+{
+	int len = vec.size();
+	vector<string> res;
+	for (int i = 0; i < len; i++)
+	{
+		res.push_back("");
+	}
+	int s = 0;
+	int flag = 0;
+	map<string, int>mymap;
+	vector<string>temp;
+	vector<int>suoyin;
+	while (flag<len)
+	{
+		s++;
+		mymap.clear();
+		for (int i = 0; i < vec.size(); ++i)
+		{
+			if (find(suoyin.begin(), suoyin.end(), i) == suoyin.end())
+			{
+				if (mymap.find(vec[i].substr(0, s)) == mymap.end())
+				{
+					mymap.insert(pair<string, int>(vec[i].substr(0, s), i));
+				}
+				else
+				{
+					mymap[vec[i].substr(0, s)] = -1;
+				}
+			}
+		}
+		if (mymap.size() > 0)
+		{
+			for (auto it = mymap.begin(); it !=mymap.end(); ++it)
+			{
+				if(it->second!=-1)	
+				{
+					suoyin.push_back(it->second);
+					res[it->second] = it->first;
+				    flag++;
+				}
+			}
+		}
+	}
+	return res;
+}
+
+void main() {
+	vector<string>vec = {"bytedance","toutiaohao","toutiaoapp","iesaweme","iestiktok"};
+	vector<string> res=test(vec);
+	for (int i = 0; i < res.size(); ++i)
+	{
+	cout << res[i] << endl;
+	}
+	system("pause");
+}
---
+++ 20180920字节跳动/3从字典和单词列表中找出所有的单词.cpp
@@ -0,0 +1,128 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+#include<map>
+using namespace std;
+
+//题目描述
+//给定一个(M*N)二维的字母词典和一组单词列表(长度K)，M为列数，
+//N为行数，从词典里找出所有的单词。
+//单词要遵循的规则是所有的字母要词典中是相邻的，可以上下相邻也可以左右相邻，
+//词典中每个字母单元只能使用一次。
+//
+//输入描述
+//第一行三个整数分别代表M N K
+//第二行 K个单词
+//第三行到第K+3行为词典
+//输出描述
+//找到的单词，每行一个
+//
+//示例1
+//输入
+//5 5 3
+//hello help high
+//p a b h m
+//f h e c p
+//c i l l h
+//b g h o n
+//h x c m l
+//输出
+//hello
+//high
+
+//k为第几个字符
+bool findword(vector<vector<char>>& vec, vector<vector<bool>> flag,string str, int i, int j,int k)
+{
+	if (flag[i][j] == true)
+		return false;
+
+	if (k < str.size() && str[k] != vec[i][j])
+		return false;
+
+	if (k < str.size() && str[k] == vec[i][j])
+	{
+		flag[i][j] = true;
+		if (k == str.size() - 1)
+			return true;
+
+		k = k + 1;
+		if (i - 1 >= 0)
+		{
+			if (findword(vec, flag, str, i - 1, j, k))
+				return true;
+		}
+		if (i + 1 < vec.size())
+		{
+			if (findword(vec, flag, str, i + 1, j, k))
+				return true;
+		}
+		if (j - 1>=0)
+		{
+			if (findword(vec, flag, str, i, j-1, k))
+				return true;
+		}
+		if (j + 1 < vec[0].size())
+		{
+			if (findword(vec, flag, str, i , j+1, k))
+				return true;
+		}
+	}
+
+	return false;
+
+	
+}
+
+vector<string> test(vector<vector<char>>& vec, vector<string>& word)
+{
+	vector<string>res;
+	vector<char> w;
+	for (int i = 0; i < word.size(); ++i)
+	{
+		w.push_back(word[i][0]);
+	}
+	vector<vector<bool>> flag;
+	for (int m = 0; m < vec.size(); ++m)
+	{
+		vector<bool>temp;
+		for (int t = 0; t < vec[m].size(); ++t)
+		{
+			temp.push_back(false);
+		}
+		flag.push_back(temp);
+	}
+	for (int i = 0; i < vec.size(); ++i)
+	{
+		for (int j = 0; j < vec[i].size(); ++j)
+		{
+			for (int k = 0; k < w.size(); ++k)
+			{
+				if (w[k] == vec[i][j])
+				{
+					if (findword(vec,flag, word[k], i, j,0) &&find(res.begin(),res.end(),word[k])==res.end())
+						res.push_back(word[k]);
+				}
+			}
+		}
+	}
+	return res;
+}
+
+void main() {
+	vector<vector<char>> vec = {
+		{'p','a','b','h','m'},
+	{'f','h','e','c','p'},
+	{'c','i','l','l','h'},
+	{'b','g','h','o','n'},
+	{'h','x','c','m','l'} };
+	vector<string>word = { "hello", "help","high" };
+
+	vector<string>res = test(vec, word);
+	for (int i = 0; i < res.size(); ++i)
+	{
+		cout << res[i] << endl;
+	}
+
+
+	system("pause");
+}
