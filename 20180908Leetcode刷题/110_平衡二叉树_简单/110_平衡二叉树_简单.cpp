92b13a5cfe09e3ee060a49fc35cc01aba46fcf14
xl <1756625653@qq.com>
2018-09-22 22:03:55 +0800

    update

---
+++ 20180908Leetcode刷题/110_平衡二叉树_简单/110_平衡二叉树_简单.cpp
@@ -0,0 +1,136 @@
+﻿#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+//给定一个二叉树，判断它是否是高度平衡的二叉树。
+//
+//本题中，一棵高度平衡二叉树定义为：
+//
+//一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+//
+//示例 1:
+//给定二叉树 [3,9,20,null,null,15,7]
+//
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//返回 true 。
+//
+//示例 2:
+//给定二叉树 [1,2,2,3,3,null,null,4,4]
+//
+//       1
+//      / \
+//     2   2
+//    / \
+//   3   3
+//  / \
+// 4   4
+//返回 false 。
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+//得到树的深度时，判断子树是否平衡
+int GetTreeDepthAndTest(TreeNode* root,bool& res)
+{
+	res = true;
+	if (!root)
+	{
+		return 0;
+	}
+	int left = 0, right = 0;
+	bool leftres = true,rightres=true;
+	if (root->left)
+		left = GetTreeDepthAndTest(root->left,leftres);
+	if (root->right)
+		right = GetTreeDepthAndTest(root->right,rightres);
+
+	if (!leftres || !rightres || abs(left-right)>1)
+	{
+		res = false;
+	}
+	return left > right ? left+1:right+1 ;
+}
+
+bool isBalanced(TreeNode* root) {
+	bool res = true;
+	GetTreeDepthAndTest(root, res);
+	return res;
+}
+
+void Qianxubianli(TreeNode* pNode)
+{
+	if (pNode == NULL)
+		return;
+
+	cout << pNode->val << " ,";
+	Qianxubianli(pNode->left);
+	Qianxubianli(pNode->right);
+}
+
+void Zhongxubianli(TreeNode* pNode)
+{
+	if (pNode == NULL)
+		return;
+
+	Zhongxubianli(pNode->left);
+	cout << pNode->val << " ,";
+	Zhongxubianli(pNode->right);
+}
+
+void Houxubianli(TreeNode* pNode)
+{
+	if (pNode == NULL)
+		return;
+
+	Houxubianli(pNode->left);
+	Houxubianli(pNode->right);
+	cout << pNode->val << " ,";
+}
+
+int main() {
+	//test1
+	TreeNode* t1 = new TreeNode(3);
+	TreeNode* t1_9 = new TreeNode(9);
+	TreeNode* t1_20 = new TreeNode(20);
+	TreeNode* t1_15 = new TreeNode(15);
+	TreeNode* t1_7 = new TreeNode(7);
+
+	t1->left = t1_9;
+	t1->right = t1_20;
+	t1_20->left = t1_15;
+	t1_20->right = t1_7;
+
+	bool a = isBalanced(t1);
+
+	//test2
+	TreeNode* t2 = new TreeNode(2);
+	TreeNode* t2_2_1 = new TreeNode(2);
+	TreeNode* t2_2_2 = new TreeNode(2);
+	TreeNode* t2_3_1 = new TreeNode(3);
+	TreeNode* t2_3_2 = new TreeNode(3);
+	TreeNode* t2_4_1 = new TreeNode(4);
+	TreeNode* t2_4_2 = new TreeNode(4);
+	t2->left = t2_2_1;
+	t2->right = t2_2_2;
+	t2_2_1->left = t2_3_1;
+	t2_2_1->right = t2_3_2;
+	t2_3_1->left = t2_4_1;
+	t2_3_1->right=t2_4_2;
+
+	bool b = isBalanced(t2);
+	cout << a << endl << b << endl;
+	//test end
+
+	system("pause");
+	return 0;
+}
---
+++ 20180908Leetcode刷题/506_相对名次_简单/506_相对名次_简单.cpp
@@ -0,0 +1,105 @@
+﻿#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<sstream>
+using namespace std;
+
+//给出 N 名运动员的成绩，找出他们的相对名次并授予前三名对应的奖牌。前三名运动员将会被分别授予 “金牌”，“银牌” 和“ 铜牌”（"Gold Medal", "Silver Medal", "Bronze Medal"）。
+//
+//(注：分数越高的选手，排名越靠前。)
+//
+//示例 1:
+//输入: [5, 4, 3, 2, 1]
+//输出: ["Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"]
+//解释: 前三名运动员的成绩为前三高的，因此将会分别被授予 “金牌”，“银牌”和“铜牌” ("Gold Medal", "Silver Medal" and "Bronze Medal").
+//余下的两名运动员，我们只需要通过他们的成绩计算将其相对名次即可。
+//
+//提示:
+//N 是一个正整数并且不会超过 10000。
+//所有运动员的成绩都不相同。
+
+//交换两值
+void Swap(vector<int>& nums, int i, int j)
+{
+	if (i == j)
+		return;
+
+	int temp = nums[i];
+	nums[i] = nums[j];
+	nums[j] = temp;
+}
+
+//将所有值从大到小排序
+void qsort(vector<int>& nums,int m,int n)
+{
+	if (n<=m)
+		return;
+
+	int pivot = nums[m], i = m-1, j =n;
+	Swap(nums, m, n);
+	do {
+		while (j>m && nums[--j] < pivot);
+		while (i<n &&nums[++i] > pivot);
+		Swap(nums, i, j);
+	} while (i < j);
+	Swap(nums, i, j);
+	Swap(nums, i, n);
+	qsort(nums, m, i - 1);
+	qsort(nums, i + 1, n);
+}
+
+vector<string> findRelativeRanks(vector<int>& nums) {
+	vector<string>res;
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		res.push_back("");
+	}
+	map<int, int>mymap;
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		mymap.insert(pair<int, int>(nums[i], i));
+	}
+	qsort(nums,0,nums.size()-1);
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		if (i == 0)
+			res[mymap[nums[i]]] = "Gold Medal";
+		else if(i==1)
+			res[mymap[nums[i]]] = "Silver Medal";
+		else if(i==2)
+			res[mymap[nums[i]]] = "Bronze Medal";
+		else
+		{
+			stringstream ss;
+			ss << i + 1;
+			string t = ss.str();
+			res[mymap[nums[i]]] = t;
+		}
+	}
+
+	return res;
+}
+
+int main() {
+	//test1
+	vector<int>nums = { 5,4,6,2,1 };
+
+	//快排测试
+	/*qsort(nums, 0, 4);
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		cout << nums[i] << endl;
+	}*/
+
+	vector<string>res = findRelativeRanks(nums);
+	for (int i = 0; i < res.size(); ++i)
+	{
+		cout << res[i] << endl;
+	}
+	//test end
+
+	system("pause");
+	return 0;
+}
