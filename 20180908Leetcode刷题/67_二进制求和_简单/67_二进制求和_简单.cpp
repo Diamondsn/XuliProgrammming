6dc9d0002dcb7af26f3922b6a6057db783a32495
xl <1756625653@qq.com>
2018-09-24 10:07:08 +0800

    update

---
+++ 20180908Leetcode刷题/67_二进制求和_简单/67_二进制求和_简单.cpp
@@ -0,0 +1,69 @@
+﻿#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include"math.h"
+#include<unordered_map>
+#include"sstream"
+#include<map>
+using namespace std;
+
+//给定两个二进制字符串，返回他们的和（用二进制表示）。
+//
+//输入为非空字符串且只包含数字 1 和 0。
+//
+//示例 1:
+//输入: a = "11", b = "1"
+//输出: "100"
+//
+//示例 2:
+//输入: a = "1010", b = "1011"
+//输出: "10101"
+
+char stradd(char a, char b, char jinwei)
+{
+	jinwei = '0';
+	if (a == '0'&& b == '0')
+		return '0';
+	else if (a == '0' && b == '1')
+		return '1';
+	else if (a == '1'&& b == '0')
+		return '1';
+	else if (a == '1'&& b == '1')
+	{
+		jinwei = '1';
+		return '0';
+	}
+}
+string addBinary(string a, string b) {
+	string longstr, shortstr;
+	if (a.size() > b.size())
+	{
+		longstr = a;
+		shortstr = b;
+	}
+	else
+	{
+		longstr = b;
+		shortstr = a;
+	}
+	string res;
+	char jinwei='0';
+	for (int i = 1; i <= shortstr.size(); ++i)
+	{
+		char num = stradd(longstr[longstr.size() - i], shortstr[shortstr.size() - i], jinwei);
+	}
+}
+
+int main() {
+	//test1
+	string res = addBinary("11", "1");
+
+	//test2
+	string res1 = addBinary("1010","1011");
+
+	cout << res << endl << res1 << endl;
+	//test end
+	system("pause");
+	return 0;
+}
---
+++ 20180908Leetcode刷题/896_单调数列_简单/896_单调数列_简单.cpp
@@ -0,0 +1,88 @@
+﻿#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include"math.h"
+#include<unordered_map>
+#include"sstream"
+#include<map>
+using namespace std;
+
+//如果数组是单调递增或单调递减的，那么它是单调的。
+//
+//如果对于所有 i <= j，A[i] <= A[j]，那么数组 A 是单调递增的。 如果对于所有 i <= j，A[i]> = A[j]，那么数组 A 是单调递减的。
+//
+//当给定的数组 A 是单调数组时返回 true，否则返回 false。
+//
+//示例 1：
+//输入：[1,2,2,3]
+//输出：true
+//
+//示例 2：
+//输入：[6,5,4,4]
+//输出：true
+//
+//示例 3：
+//输入：[1,3,2]
+//输出：false
+//
+//示例 4：
+//输入：[1,2,4,5]
+//输出：true
+//
+//示例 5：
+//输入：[1,1,1]
+//输出：true
+//
+//提示：
+//1 <= A.length <= 50000
+//-100000 <= A[i] <= 100000
+
+bool isMonotonic(vector<int>& A) {
+	if (A.size() == 0)
+		return false;
+
+	if (A.size() == 1)
+		return true;
+	int flag = 0;
+	for (int i = 0; i < A.size()-1; ++i)
+	{
+		if (flag == 0 && A[i] > A[i + 1])
+			flag = 1;
+		else if (flag == 0 && A[i] < A[i + 1])
+			flag = 2;
+
+		if (flag == 1 && A[i] < A[i + 1])
+			return false;
+		if (flag == 2 && A[i] > A[i + 1])
+			return false;
+	}
+	return true;
+}
+
+int main() {
+	//test1
+	vector<int>num = {1,2,2,3};
+	bool a = isMonotonic(num);
+
+	//test2
+	num = { 6,5,4,4 };
+	bool b = isMonotonic(num);
+
+	//test3
+	num = { 1,3,2 };
+	bool c = isMonotonic(num);
+
+	//test4
+	num = { 1,2,4,5 };
+	bool d = isMonotonic(num);
+
+	//test5
+	num = { 1,1,1 };
+	bool e = isMonotonic(num);
+
+	cout << a << endl << b << endl << c << endl << d << endl<<e<<endl;//
+    //test end
+	system("pause");
+	return 0;
+}
