2467f896fd49d7fbe0dde5d2f98b64a1192790f2
xl <1756625653@qq.com>
2018-09-27 11:58:40 +0800

    update

---
+++ 20180908Leetcode刷题/82_删除排序链表中的重复元素2_简单/82_删除排序链表中的重复元素2_简单.cpp
@@ -0,0 +1,93 @@
+#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include"math.h"
+#include<deque>
+using namespace std;
+
+//给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字。
+//
+//示例 1:
+//输入: 1->2->3->3->4->4->5
+//输出: 1->2->5
+//
+//示例 2:
+//输入: 1->1->1->2->3
+//输出: 2->3
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* deleteDuplicates(ListNode* head) {
+	if (head == NULL)
+		return NULL;
+	ListNode* newhead = NULL;
+	bool flag = false;
+
+	while (temp) {
+		
+	}
+	return newhead;
+}
+
+void LianBiaoBianLi(ListNode* root)
+{
+	if (!root)
+		return;
+	if (root)
+	{
+		cout << root->val;
+		root = root->next;
+	}
+
+	while (root)
+	{
+		cout << "->" << root->val;
+		root = root->next;
+	}
+	cout << endl;
+}
+
+int main() {
+	//test1
+	ListNode* l1 = new ListNode(1);
+	ListNode* l2 = new ListNode(2);
+	ListNode* l3 = new ListNode(3);
+	ListNode* l4 = new ListNode(3);
+	ListNode* l5 = new ListNode(4);
+	ListNode* l6 = new ListNode(4);
+	ListNode* l7 = new ListNode(5);
+
+	l1->next = l2;
+	l2->next = l3;
+	l3->next = l4;
+	l4->next = l5;
+	l5->next = l6;
+	l6->next = l7;
+
+	ListNode* PNode = deleteDuplicates(l1);
+	cout << "test1" << endl;
+	LianBiaoBianLi(PNode);
+	//test2
+	ListNode* n1 = new ListNode(1);
+	ListNode* n2 = new ListNode(1);
+	ListNode* n3 = new ListNode(1);
+	ListNode* n4 = new ListNode(2);
+	ListNode* n5 = new ListNode(3);
+	n1->next = n2;
+	n2->next = n3;
+	n3->next = n4;
+	n4->next = n5;
+
+	ListNode* node = deleteDuplicates(n1);
+	cout << "test2" << endl;
+	LianBiaoBianLi(node);
+	//test end
+
+	system("pause");
+	return 0;
+}
---
+++ 20180908Leetcode刷题/83_删除排序链表中的重复元素_简单/83_删除排序链表中的重复元素_简单.cpp
@@ -0,0 +1,92 @@
+﻿#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include"math.h"
+#include<deque>
+using namespace std;
+
+//给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+//
+//示例 1:
+//输入: 1->1->2
+//输出: 1->2
+//
+//示例 2:
+//输入: 1->1->2->3->3
+//输出: 1->2->3
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* deleteDuplicates(ListNode* head) {
+	if (head == NULL)
+		return NULL;
+	ListNode* current = head;
+	while (current)
+	{
+		if (current->next && current->next->val == current->val)
+		{
+			ListNode* pNode = current->next;
+			current->next = current->next->next;
+			delete pNode;
+		}
+		else 
+		{
+			current = current->next;
+		}
+	}
+	return head;
+}
+
+void LianBiaoBianLi(ListNode* root)
+{
+	if (!root)
+		return;
+	if (root)
+	{
+		cout << root->val;
+		root = root->next;
+	}
+
+	while (root)
+	{
+		cout << "->"<<root->val;
+		root = root->next;
+	}
+	cout << endl;
+}
+
+int main() {
+	//test1
+	ListNode* l1 = new ListNode(1);
+	ListNode* l2 = new ListNode(1);
+	ListNode* l3 = new ListNode(2);
+	l1->next = l2;
+	l2->next = l3;
+
+	ListNode* PNode = deleteDuplicates(l1);
+	cout << "test1" << endl;
+	LianBiaoBianLi(PNode);
+	//test2
+	ListNode* n1 = new ListNode(1);
+	ListNode* n2 = new ListNode(1);
+	ListNode* n3 = new ListNode(2);
+	ListNode* n4 = new ListNode(3);
+	ListNode* n5 = new ListNode(3);
+	n1->next = n2;
+	n2->next = n3;
+	n3->next = n4;
+	n4->next = n5;
+
+	ListNode* node = deleteDuplicates(n1);
+	cout << "test2" << endl;
+	LianBiaoBianLi(node);
+	//test end
+
+	system("pause");
+	return 0;
+}
