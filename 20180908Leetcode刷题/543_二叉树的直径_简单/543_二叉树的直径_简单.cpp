f8712e419a402cc206e703e892a8bb3eb476d49c
xl <1756625653@qq.com>
2018-10-01 21:31:43 +0800

    update

---
+++ 20180908Leetcode刷题/543_二叉树的直径_简单/543_二叉树的直径_简单.cpp
@@ -0,0 +1,109 @@
+﻿#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include"math.h"
+using namespace std;
+
+//给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。
+//这条路径可能穿过根结点。
+//
+//示例 :
+//给定二叉树
+//          1
+//         / \
+//        2   3
+//       / \     
+//      4   5    
+//返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。
+//
+//注意：两结点之间的路径长度是以它们之间边的数目表示。
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+int longestpath(TreeNode* root)
+{
+	if (root == NULL)return 0;
+	int a=0,b=0;
+	if(root->left)
+	   a = longestpath(root->left)+1;
+	if(root->right)
+	   b = longestpath(root->right)+1;
+	int res= (a > b ? a : b);
+	return res;
+}
+
+int diameterOfBinaryTree(TreeNode* root) {
+	if (root == NULL)return 0;
+	int leftpath = 0, rightpath = 0, leftdia = 0,rightdia=0;
+	if (root->left)
+		leftpath = longestpath(root->left) + 1;
+	if (root->right)
+		rightpath = longestpath(root->right) + 1;
+	int max = 0;
+	leftdia = diameterOfBinaryTree(root->left);
+	rightdia = diameterOfBinaryTree(root->right);
+	if (leftpath + rightpath > max)
+		max = leftpath + rightpath;
+	if (leftdia > max)
+		max = leftdia;
+	if (rightdia > max)
+		max = rightdia;
+	return max;
+}
+
+void Qianxubianli(TreeNode* pNode)
+{
+	if (pNode == NULL)
+		return;
+
+	cout << pNode->val << " ,";
+	Qianxubianli(pNode->left);
+	Qianxubianli(pNode->right);
+}
+
+void Zhongxubianli(TreeNode* pNode)
+{
+	if (pNode == NULL)
+		return;
+
+	Zhongxubianli(pNode->left);
+	cout << pNode->val << " ,";
+	Zhongxubianli(pNode->right);
+}
+
+void Houxubianli(TreeNode* pNode)
+{
+	if (pNode == NULL)
+		return;
+
+	Houxubianli(pNode->left);
+	Houxubianli(pNode->right);
+	cout << pNode->val << " ,";
+}
+
+int main() {
+	//test1
+	TreeNode* t = new TreeNode(1);
+	TreeNode* t_2 = new TreeNode(2);
+	TreeNode* t_3 = new TreeNode(3);
+	TreeNode* t_4 = new TreeNode(4);
+	TreeNode* t_5 = new TreeNode(5);
+
+	t->left = t_2;
+	t->right = t_3;
+	t_2->left = t_4;
+	t_2->right = t_5;
+
+	int a = diameterOfBinaryTree(t);//3
+	cout << a << endl;
+	//test end
+
+	system("pause");
+	return 0;
+}
---
+++ 20180908Leetcode刷题/744_寻找比目标字母大的最小字母_简单/744_寻找比目标字母大的最小字母_简单.cpp
@@ -0,0 +1,85 @@
+﻿#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include"math.h"
+using namespace std;
+
+//给定一个只包含小写字母的有序数组letters 和一个目标字母 target，寻找有序数组里面比目标字母大的最小字母。
+//
+//数组里字母的顺序是循环的。举个例子，如果目标字母target = 'z' 并且有序数组为 letters = ['a', 'b']，则答案返回 'a'。
+//
+//示例:
+//输入:
+//letters = ["c", "f", "j"]
+//target = "a"
+//输出: "c"
+//
+//输入:
+//letters = ["c", "f", "j"]
+//target = "c"
+//输出: "f"
+//
+//输入:
+//letters = ["c", "f", "j"]
+//target = "d"
+//输出: "f"
+//
+//输入:
+//letters = ["c", "f", "j"]
+//target = "g"
+//输出: "j"
+//
+//输入:
+//letters = ["c", "f", "j"]
+//target = "j"
+//输出: "c"
+//
+//输入:
+//letters = ["c", "f", "j"]
+//target = "k"
+//输出: "c"
+//
+//注:
+//letters长度范围在[2, 10000]区间内。
+//letters 仅由小写字母组成，最少包含两个不同的字母。
+//目标字母target 是一个小写字母。
+
+char nextGreatestLetter(vector<char>& letters, char target) {
+	for (int i = 0; i < letters.size(); ++i) {
+		if (letters[i] > target)return letters[i];
+	}
+	return letters[0];
+}
+
+int main() {
+	//test1
+	vector<char>letters = { 'c','f','j' };
+	char target = 'a';
+	char a = nextGreatestLetter(letters, target);
+
+	//test2
+	target = 'c';
+	char b = nextGreatestLetter(letters, target);
+
+	//test3
+	target = 'd';
+	char c = nextGreatestLetter(letters, target);
+
+	//test4
+	target = 'g';
+	char d = nextGreatestLetter(letters, target);
+
+	//test5
+	target = 'j';
+	char e = nextGreatestLetter(letters, target);
+
+	//test6
+	target = 'k';
+	char f = nextGreatestLetter(letters, target);
+	cout << a << endl << b << endl << c << endl<<d<<endl << e << endl << f << endl;
+	//test end
+
+	system("pause");
+	return 0;
+}
