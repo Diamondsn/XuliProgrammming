fe8bbe89c65e5bce59198a465c8358ad6d0994ca
xl <1756625653@qq.com>
2018-10-02 16:32:47 +0800

    update

--- 20180908Leetcode刷题/622_设计循环队列_简单/622_设计循环队列_简单.cpp
+++ 20180908Leetcode刷题/622_设计循环队列_简单/622_设计循环队列_简单.cpp
@@ -5,103 +5,90 @@
 #include"math.h"
 using namespace std;
 
-//设计实现双端队列。
-//你的实现需要支持以下操作：
+//设计你的循环队列实现。 循环队列是一种线性数据结构，其操作表现基于 FIFO（先进先出）原则并且队尾被连接在队首之后以形成一个循环。它也被称为“环形缓冲器”。
+//循环队列的一个好处是我们可以利用这个队列之前用过的空间。在一个普通队列里，一旦一个队列满了，我们就不能插入下一个元素，即使在队列前面仍有空间。但是使用循环队列，我们能使用这些空间去存储新的值。
+//你的实现应该支持如下操作：
 //
-//MyCircularDeque(k)：构造函数, 双端队列的大小为k。
-//insertFront()：将一个元素添加到双端队列头部。 如果操作成功返回 true。
-//insertLast()：将一个元素添加到双端队列尾部。如果操作成功返回 true。
-//deleteFront()：从双端队列头部删除一个元素。 如果操作成功返回 true。
-//deleteLast()：从双端队列尾部删除一个元素。如果操作成功返回 true。
-//getFront()：从双端队列头部获得一个元素。如果双端队列为空，返回 - 1。
-//getRear()：获得双端队列的最后一个元素。 如果双端队列为空，返回 - 1。
-//isEmpty()：检查双端队列是否为空。
-//isFull()：检查双端队列是否满了。
+//MyCircularQueue(k): 构造器，设置队列长度为 k 。
+//Front: 从队首获取元素。如果队列为空，返回 -1 。
+//Rear: 获取队尾元素。如果队列为空，返回 -1 。
+//enQueue(value): 向循环队列插入一个元素。如果成功插入则返回真。
+//deQueue(): 从循环队列中删除一个元素。如果成功删除则返回真。
+//isEmpty(): 检查循环队列是否为空。
+//isFull(): 检查循环队列是否已满。
 //
 //示例：
-//MyCircularDeque circularDeque = new MycircularDeque(3); // 设置容量大小为3
-//circularDeque.insertLast(1);			// 返回 true
-//circularDeque.insertLast(2);			// 返回 true
-//circularDeque.insertFront(3);			// 返回 true
-//circularDeque.insertFront(4);			// 已经满了，返回 false
-//circularDeque.getRear();  				// 返回  32
-//circularDeque.isFull();				// 返回 true
-//circularDeque.deleteLast();			// 返回 true
-//circularDeque.insertFront(4);			// 返回 true
-//circularDeque.getFront();				// 返回 4
+//MyCircularQueue circularQueue = new MycircularQueue(3); // 设置长度为3
 //
+//circularQueue.enQueue(1);  // 返回true
+//
+//circularQueue.enQueue(2);  // 返回true
+//
+//circularQueue.enQueue(3);  // 返回true
+//
+//circularQueue.enQueue(4);  // 返回false,队列已满
+//
+//circularQueue.Rear();  // 返回3
+//
+//circularQueue.isFull();  // 返回true
+//
+//circularQueue.deQueue();  // 返回true
+//
+//circularQueue.enQueue(4);  // 返回true
+//
+//circularQueue.Rear();  // 返回4
+// 
 //提示：
-//所有值的范围为[1, 1000]
-//操作次数的范围为[1, 1000]
-//请不要使用内置的双端队列库。
+//所有的值都在 1 至 1000 的范围内；
+//操作数将在 1 至 1000 的范围内；
+//请不要使用内置的队列库。
 
-class MyCircularDeque {
+class MyCircularQueue {
 public:
-	/** Initialize your data structure here. Set the size of the deque to be k. */
-	MyCircularDeque(int k) {
+	/** Initialize your data structure here. Set the size of the queue to be k. */
+	MyCircularQueue(int k) {
 		rongliang = k;
 		cur = 0;
 	}
 
-	/** Adds an item at the front of Deque. Return true if the operation is successful. */
-	bool insertFront(int value) {
+	/** Insert an element into the circular queue. Return true if the operation is successful. */
+	bool enQueue(int value) {
 		if (isFull())
 			return false;
-		vec.insert(vec.begin(), value);
+		vec.insert(vec.end(), value);
 		cur++;
 		return true;
 	}
 
-	/** Adds an item at the rear of Deque. Return true if the operation is successful. */
-	bool insertLast(int value) {
-		if (isFull())
-			return false;
-		vec.push_back(value);
-		cur++;
-		return true;
-	}
-
-	/** Deletes an item from the front of Deque. Return true if the operation is successful. */
-	bool deleteFront() {
+	/** Delete an element from the circular queue. Return true if the operation is successful. */
+	bool deQueue() {
 		if (isEmpty())
 			return false;
-
 		vec.erase(vec.begin());
 		cur--;
 		return true;
 	}
 
-	/** Deletes an item from the rear of Deque. Return true if the operation is successful. */
-	bool deleteLast() {
-		if (isEmpty())
-			return false;
-
-		vec.erase(vec.end() - 1);
-		cur--;
-		return true;
-	}
-
-	/** Get the front item from the deque. */
-	int getFront() {
+	/** Get the front item from the queue. */
+	int Front() {
 		if (isEmpty())
 			return -1;
 		return vec[0];
 	}
 
-	/** Get the last item from the deque. */
-	int getRear() {
+	/** Get the last item from the queue. */
+	int Rear() {
 		if (isEmpty())
 			return -1;
-
 		return vec[vec.size() - 1];
 	}
 
-	/** Checks whether the circular deque is empty or not. */
+	/** Checks whether the circular queue is empty or not. */
 	bool isEmpty() {
 		return cur == 0;
 	}
 
-	/** Checks whether the circular deque is full or not. */
+	/** Checks whether the circular queue is full or not. */
 	bool isFull() {
 		return cur == rongliang;
 	}
@@ -111,30 +98,28 @@ public:
 };
 
 /**
-* Your MyCircularDeque object will be instantiated and called as such:
-* MyCircularDeque obj = new MyCircularDeque(k);
-* bool param_1 = obj.insertFront(value);
-* bool param_2 = obj.insertLast(value);
-* bool param_3 = obj.deleteFront();
-* bool param_4 = obj.deleteLast();
-* int param_5 = obj.getFront();
-* int param_6 = obj.getRear();
-* bool param_7 = obj.isEmpty();
-* bool param_8 = obj.isFull();
+* Your MyCircularQueue object will be instantiated and called as such:
+* MyCircularQueue obj = new MyCircularQueue(k);
+* bool param_1 = obj.enQueue(value);
+* bool param_2 = obj.deQueue();
+* int param_3 = obj.Front();
+* int param_4 = obj.Rear();
+* bool param_5 = obj.isEmpty();
+* bool param_6 = obj.isFull();
 */
 
 int main() {
 	//test1
-	MyCircularDeque circularDeque(3); // 设置容量大小为3
-	bool a = circularDeque.insertLast(1);			// 返回 true
-	bool b = circularDeque.insertLast(2);			// 返回 true
-	bool c = circularDeque.insertFront(3);			// 返回 true
-	bool d = circularDeque.insertFront(4);			// 已经满了，返回 false
-	int e = circularDeque.getRear();  				// 返回  2
-	bool f = circularDeque.isFull();				// 返回 true
-	bool g = circularDeque.deleteLast();			// 返回 true
-	bool h = circularDeque.insertFront(4);			// 返回 true
-	int i = circularDeque.getFront();				// 返回 4
+	MyCircularQueue circularQueue(3); // 设置长度为3
+	bool a=circularQueue.enQueue(1);  // 返回true
+	bool b=circularQueue.enQueue(2);  // 返回true
+	bool c=circularQueue.enQueue(3);  // 返回true
+	bool d=circularQueue.enQueue(4);  // 返回false,队列已满
+	int e=circularQueue.Rear();  // 返回3
+	bool f=circularQueue.isFull();  // 返回true
+	bool g=circularQueue.deQueue();  // 返回true
+	bool h=circularQueue.enQueue(4);  // 返回true
+	int i=circularQueue.Rear();  // 返回4
 	cout << a << endl << b << endl << c << endl << d << endl << e << endl << f << endl << g << endl << h << endl << i << endl;
 	//test end
 	system("pause");
