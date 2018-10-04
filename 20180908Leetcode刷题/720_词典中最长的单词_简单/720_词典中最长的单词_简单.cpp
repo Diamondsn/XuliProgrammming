0f07257e88b6ed7a3ac37f0c811b849bdbd5144e
xl <1756625653@qq.com>
2018-10-04 18:23:17 +0800

    update

--- 20180908Leetcode刷题/720_词典中最长的单词_简单/720_词典中最长的单词_简单.cpp
+++ 20180908Leetcode刷题/720_词典中最长的单词_简单/720_词典中最长的单词_简单.cpp
@@ -30,7 +30,32 @@ using namespace std;
 //words[i]的长度范围为[1,30]。
 
 string longestWord(vector<string>& words) {
-
+	vector<vector<string>>tu(31);
+	for (int i = 0; i < words.size(); ++i) {
+		tu[words[i].size()].push_back(words[i]);
+	}
+	if (tu[1].size() == 0)
+		return "";
+	int suoyin = 1;
+	for (suoyin = 2; suoyin< tu.size(); ++suoyin) {
+		for (auto j = tu[suoyin].begin(); j != tu[suoyin].end(); ) {//在遍历vector时删除与插入均需更新迭代器
+			string t = j->substr(0, j->size() - 1);
+			auto it = find(tu[suoyin - 1].begin(), tu[suoyin - 1].end(), t);
+			if (it == tu[suoyin - 1].end())
+				j = tu[suoyin].erase(j);//自动指向删除后的下一个
+			else
+				j++;
+		}
+		if (tu[suoyin].size() == 0) {
+			break;
+		}
+	}
+	int lowindex = 0;
+	for (int j = 1; j < tu[suoyin - 1].size(); ++j) {//字典序比较
+		if (tu[suoyin - 1][j] < tu[suoyin - 1][lowindex])
+			lowindex = j;
+	}
+	return tu[suoyin - 1][lowindex];
 }
 
 int main() {
