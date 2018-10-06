147ecdc6e9402de790ec8864e04dcfbc4559dcdd
xl <1756625653@qq.com>
2018-10-06 15:59:33 +0800

    update

--- 20180908Leetcode刷题/387_字符串中的第一个唯一字符_简单/387_字符串中的第一个唯一字符.cpp
+++ 20180908Leetcode刷题/387_字符串中的第一个唯一字符_简单/387_字符串中的第一个唯一字符.cpp
@@ -18,7 +18,12 @@ using namespace std;
 //注意事项：您可以假定该字符串只包含小写字母。
 
 int firstUniqChar(string s) {
-
+	for (int i = 0; i < s.size(); ++i)
+	{
+		if (s.find_last_of(s[i])==s.find_first_of(s[i]))
+			return i;
+	}
+	return -1;
 }
 
 int main() {
