7340037991349c5dde0e74e09359d9f636a248ad
xl <1756625653@qq.com>
2018-10-07 14:50:30 +0800

    update

---
+++ 20180908Leetcode刷题/14_最长公共前缀_简单/14_最长公共前缀_简单.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+#include<vector>
+#include<stack>
+#include<string>
+#include<map>
+#include<math.h>
+#include<algorithm>
+using namespace std;
+
+//编写一个函数来查找字符串数组中的最长公共前缀。
+//
+//如果不存在公共前缀，返回空字符串 ""。
+//
+//示例 1:
+//输入: ["flower","flow","flight"]
+//输出: "fl"
+//
+//示例 2:
+//输入: ["dog","racecar","car"]
+//输出: ""
+//解释: 输入不存在公共前缀。
+//
+//说明:
+//所有输入只包含小写字母 a-z 。
+
+string longestCommonPrefix(vector<string>& strs) {
+
+}
+
+void main() {
+	//test1
+	vector<string>strs = { "flowers","flow","flight" };
+	string a = longestCommonPrefix(strs);
+
+	//test2
+	strs = { "dog","racecar","car" };
+	string b = longestCommonPrefix(strs);
+
+	cout << a << endl << b << endl;
+	//test end
+	system("pause");
+}
