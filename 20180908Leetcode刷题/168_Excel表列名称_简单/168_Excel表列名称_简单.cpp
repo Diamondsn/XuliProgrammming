7daa290b51b62ca283e368e6984db4e737c3fedd
xl <1756625653@qq.com>
2018-10-07 11:19:04 +0800

    update

--- 20180908Leetcode刷题/168_Excel表列名称_简单/168_Excel表列名称_简单.cpp
+++ 20180908Leetcode刷题/168_Excel表列名称_简单/168_Excel表列名称_简单.cpp
@@ -4,32 +4,52 @@
 #include<string>
 using namespace std;
 
-//给定一个仅包含大小写字母和空格 ' ' 的字符串，返回其最后一个单词的长度。
+//给定一个正整数，返回它在 Excel 表中相对应的列名称。
 //
-//如果不存在最后一个单词，请返回 0 。
+//例如，
+//    1 -> A
+//    2 -> B
+//    3 -> C
+//    ...
+//    26 -> Z
+//    27 -> AA
+//    28 -> AB 
+//    ...
+//示例 1:
 //
-//说明：一个单词是指由字母组成，但不包含任何空格的字符串。
+//输入: 1
+//输出: "A"
+//示例 2:
 //
-//示例:
-//输入: "Hello World"
-//输出: 5
+//输入: 28
+//输出: "AB"
+//示例 3:
+//
+//输入: 701
+//输出: "ZY"
 
-int lengthOfLastWord(string s) {
-	if (s.size() < 1)return 0;
-	s.erase(0, s.find_first_not_of(' '));
-	s.erase(s.find_last_not_of(' ') + 1);//以上三行为trim()去掉字符串首尾空格代码;
-	int a = s.find_last_of(' ');
-	if (a == -1)return s.size();
-	else return s.substr(a + 1).size();
+string convertToTitle(int n) {
+	if (n <= 0)return "";
+	string res;
+	vector<char>dic = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };
+	while (n > 0) {
+		int suoyin = (n-1) % 26;//c此题的映射关系26进制不能变，但是数不是从0开始记得，而是1
+		n =(n-1)/ 26;
+		res = dic[suoyin]+res;
+	}
+	return res;
 }
 
 void main() {
 	//test1
-	int a = lengthOfLastWord("Hello World");
+	string a=convertToTitle(1);
 
 	//test2
-	int b = lengthOfLastWord("a ");
-	cout << a << endl << b << endl;
+	string b = convertToTitle(28);
+
+	//test3
+	string c = convertToTitle(701);
+	cout << a << endl << b << endl << c << endl;
 	//test end
 	system("pause");
 }
---
+++ 20180908Leetcode刷题/219_存在重复元素2_简单/219_存在重复元素2_简单.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+#include<vector>
+#include<stack>
+#include<string>
+using namespace std;
+
+//给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的绝对值最大为 k。
+//
+//示例 1:
+//输入: nums = [1,2,3,1], k = 3
+//输出: true
+//
+//示例 2:
+//输入: nums = [1,0,1,1], k = 1
+//输出: true
+//
+//示例 3:
+//输入: nums = [1,2,3,1,2,3], k = 2
+//输出: false
+
+bool containsNearbyDuplicate(vector<int>& nums, int k) {
+
+}
+
+void main() {
+	//test1
+	vector<int>nums = { 1,2,3,1 };
+	bool a = containsNearbyDuplicate(nums, 3);
+
+	//test2
+	nums = { 1,0,1,1 };
+	bool b = containsNearbyDuplicate(nums, 1);
+
+	//test3
+	nums = { 1,2,3,1,2,3 };
+	bool c = containsNearbyDuplicate(nums, 2);
+
+	cout << a << endl << b << endl << c << endl;
+	//test end
+	system("pause");
+}
