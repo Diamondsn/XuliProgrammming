958267a455e4d4b1a6d2d4b443e1efbdd22a9836
xl <1756625653@qq.com>
2018-10-08 20:45:46 +0800

    update

---
+++ 20180908Leetcode刷题/190_颠倒二进制位_简单/190_颠倒二进制位_简单.cpp
@@ -0,0 +1,31 @@
+﻿#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+using namespace std;
+
+//颠倒给定的 32 位无符号整数的二进制位。
+//
+//示例:
+//输入: 43261596
+//输出: 964176192
+//解释: 43261596 的二进制表示形式为 00000010100101000001111010011100 ，
+//      返回 964176192，其二进制表示形式为 00111001011110000010100101000000 。
+//
+//进阶:
+//如果多次调用这个函数，你将如何优化你的算法？
+
+uint32_t reverseBits(uint32_t n) {
+
+}
+
+int main() {
+	//test1
+	uint32_t res = reverseBits(43261596);
+	cout << res << endl;
+	//test end
+
+	system("pause");
+	return 0;
+}
