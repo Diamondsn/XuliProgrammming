b8f5997fc5ba04d8d100d25e9d79536061a3d32f
xl <1756625653@qq.com>
2018-10-10 20:36:58 +0800

    update

---
+++ 20180908Leetcode刷题/434_字符串中的单词数_简单/434_字符串中的单词数_简单.cpp
@@ -0,0 +1,48 @@
+﻿#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+#include<stdint.h>
+#include<map>
+using namespace std;
+
+//统计字符串中的单词个数，这里的单词指的是连续的不是空格的字符。
+//
+//请注意，你可以假定字符串里不包括任何不可打印的字符。
+//
+//示例:
+//输入: "Hello, my name is John"
+//输出: 5
+
+int countSegments(string s) {
+	int count = 0;
+	int res = 0;
+	for (int i = 0; i < s.size(); ++i) {
+		if (s[i] != ' ')
+			count++;
+		else{
+			if (count != 0)
+				res++;
+			count = 0;
+		}
+	}
+	if (count != 0)res++;
+	return res;
+}
+
+int main() {
+	//test1
+	int a = countSegments("Hello, my name is John");
+
+	//test2
+	int b = countSegments("");
+
+	//test3
+	int c = countSegments("     test");
+	cout << a << endl << b << endl << c << endl;
+	//test end
+
+	system("pause");
+	return 0;
+}
