b62717ea4a63cdbfa0bb4815e2eb98d8d1c32a02
ty2 <1756625653@qq.com>
2018-10-11 09:24:00 +0800

    update

--- 20180908Leetcode刷题/680_验证回文字符串2_简单/680_验证回文字符串2_简单.cpp
+++ 20180908Leetcode刷题/680_验证回文字符串2_简单/680_验证回文字符串2_简单.cpp
@@ -20,8 +20,64 @@ using namespace std;
 //注意:
 //字符串只包含从 a-z 的小写字母。字符串的最大长度是50000。
 
-bool validPalindrome(string s) {
+bool isHuiWen(string s){
+	int left = 0, right = s.size() - 1;
+	while (left <= right){
+		if (s[left] != s[right])
+			return false;
+		else{
+			left++;
+			right--;
+		}
+	}
+	return true;
+}
 
+//注意:此种方法无法通过test3，在删除哪一个字符时有判断的先后顺序，可能导致错误
+//bool validPalindrome(string s) {
+//	int left = 0, right = s.size() - 1;
+//	int cur = 1;
+//	while (left <= right){
+//		if (s[left] == s[right])
+//		{
+//			left++;
+//			right--;
+//		}
+//		else{
+//			if (cur && left + 1 < s.size() && s[left + 1] == s[right]){//如果这两种情况都能满足
+//				cur--;
+//				left += 2;
+//				right--;
+//			}
+//			else if (cur && right - 1 >= 0 && s[left] == s[right - 1])
+//			{
+//				cur--;
+//				right -= 2;
+//				left++;
+//			}
+//			else
+//				return false;
+//		}
+//	}
+//	return true;
+//}
+
+bool validPalindrome(string s) {
+	int left = 0, right = s.size() - 1;
+	while (left <= right){
+		if (s[left] == s[right])
+		{
+			left++;
+			right--;
+		}
+		else{
+			if (isHuiWen(s.substr(left, right - left)) || isHuiWen(s.substr(left + 1, right - left)))
+				return true;
+			else
+				return false;
+		}
+	}
+	return true;
 }
 
 int main() {
@@ -31,7 +87,9 @@ int main() {
 	//test2
 	bool b = validPalindrome("abca");
 
-	cout << a << endl<<b<<endl;
+	//test3
+	bool c = validPalindrome("aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga");
+	cout << a << endl << b << endl << c << endl;
 	//test end
 
 	system("pause");
