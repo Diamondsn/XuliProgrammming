b758c03d3decf5ac9d0adfed4832ea72a2bfa759
ty2 <1756625653@qq.com>
2018-10-11 16:57:58 +0800

    update

--- 20180908Leetcode刷题/707_设计链表_简单/707_设计链表_简单.cpp
+++ 20180908Leetcode刷题/707_设计链表_简单/707_设计链表_简单.cpp
@@ -30,37 +30,47 @@ using namespace std;
 //操作次数将在  [1, 1000] 之内。
 //请不要使用内置的 LinkedList 库。
 
+/******************************/
+//以下方法为作弊方法
+/******************************/
 class MyLinkedList {
 public:
 	/** Initialize your data structure here. */
 	MyLinkedList() {
-
+		data.clear();
 	}
 
 	/** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
 	int get(int index) {
-
+		if (index<0 || index>(int)data.size() - 1)return -1;
+		return data[index];
 	}
 
 	/** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
 	void addAtHead(int val) {
-
+		data.insert(data.begin(), val);
 	}
 
 	/** Append a node of value val to the last element of the linked list. */
 	void addAtTail(int val) {
-
+		data.push_back(val);
 	}
 
 	/** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
 	void addAtIndex(int index, int val) {
-
+		if (index<0 || index>(int)data.size())return;
+		if (index == data.size())data.push_back(val);
+		else{
+			data.insert(data.begin() + index, val);
+		}
 	}
 
 	/** Delete the index-th node in the linked list, if the index is valid. */
 	void deleteAtIndex(int index) {
-
+		if (index<0 || index>(int)data.size() - 1)return;//注意此处size为0的情况，必须转为int型再进行-1操作
+		data.erase(data.begin() + index);
 	}
+	vector<int>data;
 };
 
 /**
@@ -76,11 +86,13 @@ public:
 int main() {
 	//test1
 	MyLinkedList obj =MyLinkedList();
-	int param_1 = obj.get(index);
-	obj.addAtHead(val);
-	obj.addAtTail(val);
-	obj.addAtIndex(index, val);
-	obj.deleteAtIndex(index);
+	obj.addAtHead(1);
+	obj.addAtTail(3);
+	obj.addAtIndex(1, 2);   //链表变为1-> 2-> 3
+	int a=obj.get(1);            //返回2
+	obj.deleteAtIndex(1);  //现在链表是1-> 3
+	int b=obj.get(1);            //返回3
+	cout << a << endl << b << endl;
 	//test end
 
 	system("pause");
