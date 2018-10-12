65eb239da93d0fa91dac811377c3eca2062b788a
xl <1756625653@qq.com>
2018-10-12 21:56:10 +0800

    update

--- 20180908Leetcode刷题/338_比特位计数_中等/338_比特位计数_中等.cpp
+++ 20180908Leetcode刷题/338_比特位计数_中等/338_比特位计数_中等.cpp
@@ -23,42 +23,52 @@ using namespace std;
 //要求算法的空间复杂度为O(n)。
 //你能进一步完善解法吗？要求在C++或任何其他语言中不使用任何内置函数（如 C++ 中的 __builtin_popcount）来执行此操作。
 
-vector<vector<int>> subsets(vector<int>& nums) {
-	vector<vector<int>>res, sub;
-	vector<int>temp;
-	res.push_back(temp);
-	if (nums.size() == 0)
-		return res;
-	if (nums.size() == 1)
-	{
-		temp.push_back(nums[0]);
-		res.push_back(temp);
-		return res;
-	}
+//此题方法为巧法，采用动态规划，但思路较难想到
+//思路分析
+//十进制数　　　　　　　　二进制数　　　　　　　　1的个数
+//
+//　　1　　　　　　　　　　　　1　　　　　　　　　　   1
+//
+//　　2　　　　　　　　　　　　10　　　　　　　　　　 1
+//
+//　　3　　　　　　　　　　　　11　　　　　　　　　　  2 
+//
+//　　4　　　　　　　　　　　　100　　　　　　　　　　1
+//
+//　　5　　　　　　　　　　　　101　　　　　　　　　　 2
+//
+//　　6　　　　　　　　　　　　110　　　　　　　　　　  2
+//
+//　　7　　　　　　　　　　　　111　　　　　　　　　　  3
+//
+//看上面的一系列数字的二进制中1的个数：
+//只需考虑数的最末二进制位和最末位前的位
+//最末以前的位有的1的数量已经求得，只需看该数的最末位是否是1
 
-	vector<int>copy = nums;
-	copy.erase(copy.begin());
-	sub = subsets(copy);
-	for (int i = 0; i < sub.size(); ++i) {
-		temp = sub[i];
-		temp.insert(temp.begin(), nums[0]);
-		res.push_back(temp);
-		if (sub[i].size() > 0)
-			res.push_back(sub[i]);
+//注意:笨办法求解一个数的1的位数应该
+//int count = 0;//count代表1的个数，即答案
+//while (i) {//i为要求的数
+//	i = i&(i - 1);//i-1会将右边第一位1变为0，右边第一位1后所有0变成1，&操作后直接消化掉最右一位1
+//	count++;
+//}
+
+vector<int> countBits(int num) {
+	vector<int>res(num+1,0);
+	for (int i = 1; i <= num; ++i) {
+		res[i] = res[i >> 1] + (i & 1);
 	}
 	return res;
 }
 
 int main() {
 	//test1
-	vector<int>nums = { 1,2,3 };
-	vector<vector<int>>res = subsets(nums);
-	for (int i = 0; i < res.size(); ++i) {
-		for (int j = 0; j < res[i].size(); ++j) {
-			cout << res[i][j] << ",";
-		}
-		cout << endl;
-	}
+	vector<int>res = countBits(2);
+	for (int i = 0; i < res.size(); ++i)cout << res[i] << ",";
+	cout << endl;
+
+	//test2
+	res = countBits(5);
+	for (int i = 0; i < res.size(); ++i)cout << res[i] << ",";
 	//test end
 
 	system("pause");
