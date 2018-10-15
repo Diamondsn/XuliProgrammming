1a1d106db965d60269f23b68d0ae854099ed1d89
ty2 <1756625653@qq.com>
2018-10-15 14:47:16 +0800

    update

---
+++ 20180908Leetcode刷题/109_有序链表转换为二叉搜索树_中等/109_有序链表转换二叉搜索树_中等.cpp
@@ -0,0 +1,98 @@
+﻿#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include"math.h"
+#include<list>
+#include<map>
+#include<set>
+#include<stack>
+using namespace std;
+
+//给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+//
+//本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+//
+//示例:
+//给定的有序链表： [-10, -3, 0, 5, 9],
+//一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
+//
+//      0
+//     / \
+//   -3   9
+//   /   /
+// -10  5
+
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+TreeNode* sortedListToBST(ListNode* head) {
+
+}
+
+void Qianxubianli(TreeNode* pNode)
+{
+	if (pNode == NULL)
+		return;
+
+	cout << pNode->val << ",";
+	Qianxubianli(pNode->left);
+	Qianxubianli(pNode->right);
+}
+
+void Zhongxubianli(TreeNode* pNode)
+{
+	if (pNode == NULL)
+		return;
+
+	Zhongxubianli(pNode->left);
+	cout << pNode->val << ",";
+	Zhongxubianli(pNode->right);
+}
+
+void Houxubianli(TreeNode* pNode)
+{
+	if (pNode == NULL)
+		return;
+
+	Houxubianli(pNode->left);
+	Houxubianli(pNode->right);
+	cout << pNode->val << ",";
+}
+
+void LianBiaoBianLi(ListNode* root) {
+	while (root) {
+		cout << root->val << "->";
+		root = root->next;
+	}
+}
+
+int main() {
+	//test1
+	ListNode* l = new ListNode(-10);
+	ListNode* l3 = new ListNode(-3);
+	ListNode* l0 = new ListNode(0);
+	ListNode* l5 = new ListNode(5);
+	ListNode* l9 = new ListNode(9);
+	l->next = l3;
+	l3->next = l0;
+	l0->next = l5;
+	l5->next = l9;
+	TreeNode* res = sortedListToBST(l);
+	Qianxubianli(res);
+	//test end
+
+	system("pause");
+	return 0;
+}
