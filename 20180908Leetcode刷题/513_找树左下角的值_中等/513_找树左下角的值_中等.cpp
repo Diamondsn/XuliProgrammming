78775027fbcce00191d58952be6fafc9c43952e3
xl <1756625653@qq.com>
2018-10-16 09:29:00 +0800

    update

---
+++ 20180908Leetcode刷题/513_找树左下角的值_中等/513_找树左下角的值_中等.cpp
@@ -0,0 +1,84 @@
+﻿#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include"math.h"
+using namespace std;
+
+//给定一个二叉树，在树的最后一行找到最左边的值。
+//
+//示例 1:
+//输入:
+//
+//    2
+//   / \
+//  1   3
+//
+//输出:
+//1
+// 
+//示例 2:
+//输入:
+//
+//        1
+//       / \
+//      2   3
+//     /   / \
+//    4   5   6
+//       /
+//      7
+//
+//输出:
+//7
+// 
+//注意: 您可以假设树（即给定的根节点）不为 NULL。
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+int findBottomLeftValue(TreeNode* root) {
+
+}
+
+void Qianxubianli(TreeNode* pNode)
+{
+	if (pNode == NULL)
+		return;
+
+	cout << pNode->val << " ,";
+	Qianxubianli(pNode->left);
+	Qianxubianli(pNode->right);
+}
+
+void Zhongxubianli(TreeNode* pNode)
+{
+	if (pNode == NULL)
+		return;
+
+	Zhongxubianli(pNode->left);
+	cout << pNode->val << " ,";
+	Zhongxubianli(pNode->right);
+}
+
+void Houxubianli(TreeNode* pNode)
+{
+	if (pNode == NULL)
+		return;
+
+	Houxubianli(pNode->left);
+	Houxubianli(pNode->right);
+	cout << pNode->val << " ,";
+}
+
+int main() {
+	//test1
+	
+	//test end
+
+	system("pause");
+	return 0;
+}
