f6bbb1655c0b1f3d0e6282cf4ebeb9b80a107712
xl <1756625653@qq.com>
2018-10-17 21:48:15 +0800

    update

--- 20180908Leetcode刷题/287_寻找重复数_中等/287_寻找重复数_中等.cpp
+++ 20180908Leetcode刷题/287_寻找重复数_中等/287_寻找重复数_中等.cpp
@@ -22,8 +22,87 @@ using namespace std;
 //时间复杂度小于 O(n2) 。
 //数组中只有一个重复的数字，但它可能不止重复出现一次。
 
-int findDuplicate(vector<int>& nums) {
+//此题用于面试
+//找出连续数组但乱序中唯一出现多次的数
+//https://segmentfault.com/a/1190000003817671
 
+/**************************************/
+//1.使用hash表进行查询，空间O(n)，时间O(n),hash插入和查询需要时间
+
+//2.从左边查询一个数和右边查询，看索引是不是一样，时间O(n^2)
+
+//3.先排序再遍历，更改原始数组，时间O(nlogn)
+
+//4.二分法进行查找，使用抽屉原理，n个盒子放n+1双袜子，肯定有一个盒子里放了两双,时间O(nLogn)
+
+//5.数组索引和数组内存的值存在映射关系，如果存在两个索引映射到同一个数，即通过索引去轮询即形成了环，
+//该问题转换为寻找环的交叉点的问题，应该使用快慢指针法
+/**************************************/
+
+//以下使用二分法和抽屉原理
+//int findDuplicate(vector<int>& nums) {
+//	int left = 1,right = nums.size() - 1;
+//	while (left <= right) {
+//		int mid = (left + right) / 2,count=0;
+//		for (int t : nums) {
+//			if (t <= mid)
+//				count++;
+//		}
+//		if (count <= mid)
+//			left = mid + 1;
+//		else
+//			right = mid - 1;
+//	}
+//	return left;
+//}
+
+//以下 为二分法另解
+//int findDuplicate(vector<int>& nums) {
+//	int left = 1, right = nums.size() - 1;
+//	while (left <= right) {
+//		int mid = (left + right) / 2;
+//		int l = 0, m = 0, r = 0;
+//		for (int t : nums) {
+//			if (t == mid)m++;
+//			else if (t >= left && t < mid)l++;
+//			else if (t > mid && t <= right)r++;
+//		}
+//		if (m > 1)return mid;
+//		if (l < r)
+//			left = mid + 1;
+//		else
+//			right = mid - 1;
+//	}
+//}
+
+//快慢指针法,时间复杂度O(n)
+//int findDuplicate(vector<int>& nums) {
+//	int kuai = 0, man = 0;
+//	do {
+//		kuai = nums[nums[kuai]];
+//		man = nums[man];
+//	} while (kuai != man);
+//	int res = 0;//需要从头开始
+//	while (res!=man) {
+//		res = nums[res];
+//		man = nums[man];
+//	}
+//	return res;
+//}
+
+//快慢指针法另解
+int findDuplicate(vector<int>& nums) {
+	int kuai = 0, man = 0;
+	do {
+		kuai = nums[nums[kuai]];
+		man = nums[man];
+	} while (nums[kuai] != nums[man]);
+	int res = 0;//需要从头开始
+	while (nums[res]!=nums[man]) {
+		res = nums[res];
+		man = nums[man];
+	}
+	return nums[res];
 }
 
 int main() {
