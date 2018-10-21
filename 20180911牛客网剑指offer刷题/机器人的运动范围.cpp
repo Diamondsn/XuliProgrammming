4b5c3f780f7399c00ce634799fca367ddf6490b5
xl <1756625653@qq.com>
2018-10-21 16:07:41 +0800

    update

---
+++ 20180911牛客网剑指offer刷题/机器人的运动范围.cpp
@@ -0,0 +1,57 @@
+﻿#include<string>
+#include<vector>
+#include<deque>
+#include<iostream>
+#include<algorithm>
+#include"math.h"
+#include<set>
+using namespace std;
+
+//题目描述
+//地上有一个m行和n列的方格。一个机器人从坐标0,0的格子开始移动，
+//每一次只能向左，右，上，下四个方向移动一格，但是不能进入行坐标和列坐标的数位之和大于k的格子。 
+//例如，当k为18时，机器人能够进入方格（35,37），因为3+5+3+7 = 18。
+//但是，它不能进入方格（35,38），因为3+5+3+8 = 19。请问该机器人能够达到多少个格子？
+
+bool fit(int curx, int cury, int threshold) {
+	int count = 0;
+	while (curx) {
+		count += (curx % 10);
+		curx /= 10;
+	}
+	while (cury) {
+		count += (cury % 10);
+		cury /= 10;
+	}
+	return count <= threshold;
+}
+
+void dfs(set<pair<int, int>>& myset,int curx,int cury,int &maxrow,int &maxcol,int &threshold) {
+	if (curx<0 || cury<0 || curx>maxrow || cury>maxcol || !fit(curx, cury, threshold))return;
+	myset.insert(pair<int, int>(curx, cury));
+	if(myset.find(pair<int,int>(curx-1,cury))==myset.end())//加入判断，以减少不必要的计算
+	dfs(myset, curx - 1, cury, maxrow, maxcol, threshold);
+	if (myset.find(pair<int, int>(curx + 1, cury)) == myset.end())
+	dfs(myset, curx + 1, cury, maxrow, maxcol, threshold);
+	if (myset.find(pair<int, int>(curx, cury-1)) == myset.end())
+	dfs(myset, curx, cury - 1, maxrow, maxcol, threshold);
+	if (myset.find(pair<int, int>(curx, cury+1)) == myset.end())
+	dfs(myset, curx, cury + 1, maxrow, maxcol, threshold);
+}
+
+int movingCount(int threshold, int rows, int cols)
+{
+	int maxrow = rows - 1, maxcol = cols - 1;
+	set<pair<int, int>>myset;
+	dfs(myset, 0, 0, maxrow, maxcol, threshold);
+	return myset.size();
+}
+
+int main() {
+	//test 1
+	int a = movingCount(3,5,5);
+	cout << a << endl;
+	//test end
+	system("pause");
+	return 0;
+}
---
+++ 20180911牛客网剑指offer刷题/矩阵中的路径.cpp
@@ -0,0 +1,63 @@
+﻿#include<string>
+#include<vector>
+#include<deque>
+#include<iostream>
+#include<algorithm>
+#include"math.h"
+#include<set>
+using namespace std;
+
+//题目描述
+//请设计一个函数，用来判断在一个矩阵中是否存在一条包含某字符串所有字符的路径。
+//路径可以从矩阵中的任意一个格子开始，每一步可以在矩阵中向左，向右，向上，向下移动一个格子。
+//如果一条路径经过了矩阵中的某一个格子，则之后不能再次进入这个格子。 
+//例如 a b c e s f c s a d e e 这样的3 X 4 矩阵中包含一条字符串"bcced"的路径，
+//但是矩阵中不包含"abcb"路径，因为字符串的第一个字符b占据了矩阵中的第一行第二个格子之后，路径不能再次进入该格子。
+
+bool dfs(char* matrix, vector<bool>flag, int rows, int cols, int i, int j, char* str, int m) {
+	if (matrix[cols*i + j] != str[m])
+		return false;
+	else{
+		flag[cols*i + j] = true;
+		if(m==int(strlen(str)-1))
+		 return true;
+		else {
+			bool a = false, b = false, c = false, d = false;
+			if (i - 1 >= 0 && !flag[cols*(i - 1) + j])
+				a = dfs(matrix, flag, rows, cols, i - 1, j, str, m + 1);
+			if (i + 1 < rows &&!flag[cols*(i + 1) + j])
+				b = dfs(matrix, flag, rows, cols, i + 1, j, str, m + 1);
+			if (j - 1 >= 0 && !flag[cols*i + j - 1])
+				c = dfs(matrix, flag, rows, cols, i, j - 1, str, m + 1);
+			if (j + 1 < cols && !flag[cols*i + j + 1])
+				d = dfs(matrix, flag, rows, cols, i, j + 1, str, m + 1);
+			if (a || b || c || d)return true;
+			else 
+				return false;
+		}
+	}
+}
+
+bool hasPath(char* matrix, int rows, int cols, char* str)
+{
+	vector<bool>flag(rows*cols, false);
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			if(dfs(matrix, flag, rows, cols, i, j, str, 0))return true;
+		}
+	}
+	return false;
+}
+
+int main() {
+	//test 1
+	char matrix[12] = {'a','b','c' ,'e',' s', 'f', 'c', 's', 'a', 'd' ,'e', 'e' };
+	bool a = hasPath(matrix, 3, 4, "bcced");
+
+	//test2
+	bool b = hasPath(matrix, 3, 4, "abcb");
+	cout << a << endl << b << endl;
+	//test end
+	system("pause");
+	return 0;
+}
