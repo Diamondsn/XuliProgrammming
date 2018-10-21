e2d452db05f27895ff1e7088c74ce0b7fd29b2ae
xl <1756625653@qq.com>
2018-10-21 17:11:28 +0800

    update

---
+++ 20180911牛客网剑指offer刷题/数据流中的中位数.cpp
@@ -0,0 +1,42 @@
+﻿#include<string>
+#include<vector>
+#include<deque>
+#include<iostream>
+#include<algorithm>
+#include"math.h"
+#include<set>
+#include<deque>
+using namespace std;
+
+//题目描述
+//如何得到一个数据流中的中位数？如果从数据流中读出奇数个数值，
+//那么中位数就是所有数值排序之后位于中间的数值。如果从数据流中读出偶数个数值，
+//那么中位数就是所有数值排序之后中间两个数的平均值。
+//我们使用Insert()方法读取数据流，使用GetMedian()方法获取当前读取数据的中位数。
+
+class Solution {
+public:
+	void Insert(int num)
+	{
+
+	}
+
+	double GetMedian()
+	{
+
+	}
+
+};
+
+int main() {
+	//test 1
+	Solution sol = Solution();
+	vector<int>num = {9,8,7,6,5,4,3,2,1};
+	for (int i = 0; i < num.size(); ++i) {
+		sol.Insert(num[i]);
+		cout << sol.GetMedian()<<endl;
+	}
+	//test end
+	system("pause");
+	return 0;
+}
--- 20180911牛客网剑指offer刷题/滑动窗口的最大值.cpp
+++ 20180911牛客网剑指offer刷题/滑动窗口的最大值.cpp
@@ -5,6 +5,7 @@
 #include<algorithm>
 #include"math.h"
 #include<set>
+#include<deque>
 using namespace std;
 
 //题目描述
@@ -14,14 +15,29 @@ using namespace std;
 //{ [2,3,4],2,6,2,5,1 }，{ 2,[3,4,2],6,2,5,1 }，{ 2,3,[4,2,6],2,5,1 }，{ 2,3,4,[2,6,2],5,1 }，
 //{ 2,3,4,2,[6,2,5],1 }，{ 2,3,4,2,6,[2,5,1] }。
 
+//思路:用双端队列，O(n)的时间复杂度和空间复杂度
 vector<int> maxInWindows(const vector<int>& num, unsigned int size)
 {
-
+	vector<int>res;
+	deque<int>mydeque;//存储索引，同时索引映射的值是从大到小排列的
+	for (int i = 0; i < num.size(); ++i) {
+		while (mydeque.size() && num[mydeque.back()] < num[i])mydeque.pop_back();//从后往前剔除比他小的数
+		while (mydeque.size() && i - mydeque.front() + 1 > size)mydeque.pop_front();//第一个数超出了窗口，删除
+		mydeque.push_back(i);
+		if (i >= size-1)res.push_back(num[mydeque.front()]);//当轮询到窗口右端的值才输出
+	}
+	return res;
 }
 
 int main() {
 	//test 1
-	
+	vector<int>num = { 2,3,4,2,6,2,5,1 };
+	int size = 1;
+	vector<int>res = maxInWindows(num, size);
+	for (int t : res) {
+		cout << t << ",";
+	}
+	cout << endl;
 	//test end
 	system("pause");
 	return 0;
