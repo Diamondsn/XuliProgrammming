6b3598e05662bee6f96f335a4c69ac30710bfbf6
ty2 <1756625653@qq.com>
2018-10-23 15:16:47 +0800

    update

--- 20180908Leetcode刷题/0_腾讯探索/数组与字符串/两个排序数组的中位数.cpp
+++ 20180908Leetcode刷题/0_腾讯探索/数组与字符串/两个排序数组的中位数.cpp
@@ -23,8 +23,31 @@ using namespace std;
 //nums2 = [3, 4]
 //中位数是 (2 + 3)/2 = 2.5
 
-double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+double findKth(vector<int>& nums1, int i, vector<int>& nums2, int j, int k){
+	//首先让数组1的长度小于等于数组2的长度
+	if (nums1.size() - i > nums2.size() - j)
+		return findKth(nums2, j, nums1, i, k);
+	//终止条件
+	if (nums1.size() == i)
+		return nums2[j + k - 1];
+	if (k == 1)
+		return min(nums1[i], nums2[j]);
+	int pa = min(i + k / 2, (int)nums1.size()), pb = j + k - pa + i;
+	if (nums1[pa-1] < nums2[pb-1])
+		return findKth(nums1, pa, nums2, j, k - pa + i);
+	else if (nums1[pa-1]>nums2[pb-1])
+		return findKth(nums1, i, nums2, pb, k - pb + j);
+	else
+		return nums1[pa-1];
 
+}
+
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+	int a = nums1.size(), b = nums2.size();
+	if (((a + b) & 1) == 0)//偶数
+		return (findKth(nums1, 0, nums2, 0, (a + b) / 2) + findKth(nums1, 0, nums2, 0, (a + b) / 2 + 1)) / 2.0;
+	else
+		return findKth(nums1, 0, nums2, 0, (a + b) / 2+1);
 }
 
 int main() {
@@ -36,7 +59,7 @@ int main() {
 	nums1 = { 1, 2 };
 	nums2 = { 3, 4 };
 	double b = findMedianSortedArrays(nums1, nums2);
-	cout << a << b << endl;
+	cout << a <<endl<< b << endl;
 	//test end
 
 	system("pause");
---
+++ 20180908Leetcode刷题/0_腾讯探索/数组与字符串/最长回文子串.cpp
@@ -0,0 +1,36 @@
+﻿#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include"math.h"
+#include<queue>
+#include<map>
+using namespace std;
+
+//给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为1000。
+//
+//示例 1：
+//输入: "babad"
+//输出: "bab"
+//注意: "aba"也是一个有效答案。
+//
+//示例 2：
+//输入: "cbbd"
+//输出: "bb"
+
+string longestPalindrome(string s) {
+
+}
+
+int main() {
+	//test1
+	string a = longestPalindrome("babad");
+
+	//test2
+	string b = longestPalindrome("cbbd");
+	cout << a << endl << b << endl;
+	//test end
+
+	system("pause");
+	return 0;
+}
