fe1d715218c83c46463701da266e97f332efcf8c
ty2 <1756625653@qq.com>
2018-10-23 11:18:56 +0800

    update

---
+++ 20180908Leetcode刷题/0_腾讯探索/数组与字符串/两个排序数组的中位数.cpp
@@ -0,0 +1,44 @@
+﻿#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include"math.h"
+#include<queue>
+#include<map>
+using namespace std;
+
+//给定两个大小为 m 和 n 的有序数组 nums1 和 nums2 。
+//
+//请找出这两个有序数组的中位数。要求算法的时间复杂度为 O(log (m+n)) 。
+//
+//你可以假设 nums1 和 nums2 不同时为空。
+//
+//示例 1:
+//nums1 = [1, 3]
+//nums2 = [2]
+//中位数是 2.0
+//
+//示例 2:
+//nums1 = [1, 2]
+//nums2 = [3, 4]
+//中位数是 (2 + 3)/2 = 2.5
+
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+}
+
+int main() {
+	//test1
+	vector<int>nums1 = { 1, 3 }, nums2 = { 2 };
+	double a = findMedianSortedArrays(nums1, nums2);
+
+	//test2
+	nums1 = { 1, 2 };
+	nums2 = { 3, 4 };
+	double b = findMedianSortedArrays(nums1, nums2);
+	cout << a << b << endl;
+	//test end
+
+	system("pause");
+	return 0;
+}
---
+++ 20180908Leetcode刷题/0_腾讯探索/数组与字符串/两数之和.cpp
@@ -0,0 +1,47 @@
+﻿#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include"math.h"
+#include<queue>
+#include<map>
+using namespace std;
+
+//给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+//
+//你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+//
+//示例:
+//给定 nums = [2, 7, 11, 15], target = 9
+//因为 nums[0] + nums[1] = 2 + 7 = 9
+//所以返回 [0, 1]
+
+vector<int> twoSum(vector<int>& nums, int target) {
+	map<int, int>hash;//第一个值是后续需要查找的值，第二个是之前的索引
+	vector<int>res;
+	for (int i = 0; i < nums.size(); ++i){
+		auto iter = hash.find(nums[i]);
+		if (iter != hash.end())
+		{
+			res.push_back(iter->second);
+			res.push_back(i);
+			return res;
+		}
+		else
+			hash.insert(pair<int, int>(target - nums[i], i));
+	}
+	return res;
+}
+
+int main() {
+	//test1
+	vector<int>nums = { 2, 7, 11, 15 };
+	int target = 9;
+	vector<int>res = twoSum(nums, target);
+	for (int t : res)
+		cout << t << ",";
+	//test end
+
+	system("pause");
+	return 0;
+}
