a76db0db31ec212c66817cb3dc5e5ad4e1f243d7
ty2 <1756625653@qq.com>
2018-10-26 11:32:01 +0800

    update

---
+++ 20180908Leetcode刷题/0_腾讯探索/链表突击/合并K个排序链表.cpp
@@ -0,0 +1,47 @@
+﻿#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include"math.h"
+#include<queue>
+#include<map>
+#include<set>
+using namespace std;
+
+//合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。
+//
+//示例:
+//输入:
+//[
+//  1->4->5,
+//  1->3->4,
+//  2->6
+//]
+//输出: 1->1->2->3->4->4->5->6
+
+//Definition for singly-linked list.
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* mergeKLists(vector<ListNode*>& lists) {
+
+}
+
+void LianBiaoBianli(ListNode* head){
+	while (head){
+		cout << head->val << "->";
+		head = head->next;
+	}
+}
+
+int main() {
+	//test1
+	
+	//test end
+
+	system("pause");
+	return 0;
+}
