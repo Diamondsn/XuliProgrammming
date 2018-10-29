fe3575b2c3ad24cdfd3063d779b0349c0ae82bbc
ty2 <1756625653@qq.com>
2018-10-29 09:39:22 +0800

    update

---
+++ 20180908Leetcode刷题/515_在每个树行中找最大值_中等/515_在每个树行中找最大值_中等.cpp
@@ -0,0 +1,125 @@
+﻿#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include"math.h"
+#include<list>
+using namespace std;
+
+//您需要在二叉树的每一行中找到最大的值。
+//
+//示例：
+//
+//输入: 
+//          1
+//         / \
+//        3   2
+//       / \   \  
+//      5   3   9 
+//输出: [1, 3, 9]
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+vector<int> largestValues(TreeNode* root) {
+	vector<int>res;
+	if (!root)return res;
+	list<TreeNode*>mylist;
+	mylist.push_back(root);
+	int shang = 1, xia = 0;
+	int curmax=INT_MIN;
+	while (!mylist.empty()){
+		TreeNode* pNode=mylist.front();
+		mylist.pop_front();
+		if (pNode->val > curmax)
+			curmax = pNode->val;
+	
+		shang--;
+		if (pNode->left){
+			mylist.push_back(pNode->left);
+			xia++;
+		}
+		if (pNode->right){
+			mylist.push_back(pNode->right);
+			xia++;
+		}
+		if (shang == 0){
+			res.push_back(curmax);
+			curmax = INT_MIN;
+			shang = xia;
+			xia = 0;
+		}
+	}
+	return res;
+}
+
+void Qianxubianli(TreeNode* pNode)
+{
+	if (pNode == NULL)
+		return;
+
+	cout << pNode->val << " ,";
+	Qianxubianli(pNode->left);
+	Qianxubianli(pNode->right);
+}
+
+void Zhongxubianli(TreeNode* pNode)
+{
+	if (pNode == NULL)
+		return;
+
+	Zhongxubianli(pNode->left);
+	cout << pNode->val << " ,";
+	Zhongxubianli(pNode->right);
+}
+
+void Houxubianli(TreeNode* pNode)
+{
+	if (pNode == NULL)
+		return;
+
+	Houxubianli(pNode->left);
+	Houxubianli(pNode->right);
+	cout << pNode->val << " ,";
+}
+
+int main() {
+	//test1
+	TreeNode* t = new TreeNode(1);
+	TreeNode* t_3 = new TreeNode(3);
+	TreeNode* t_2 = new TreeNode(2);
+	TreeNode* t_5 = new TreeNode(5);
+	TreeNode* t_3_2 = new TreeNode(3);
+	TreeNode* t_9 = new TreeNode(9);
+
+	t->left = t_3;
+	t->right = t_2;
+	t_3->left = t_5;
+	t_3->right = t_3_2;
+	t_2->right = t_9;
+
+	vector<int>vec = largestValues(t);
+	for (int t : vec){
+		cout << t << endl;
+	}
+
+	//test2
+	cout << "test2" << endl;
+	TreeNode* n = new TreeNode(43);
+	TreeNode* n_35 = new TreeNode(35);
+	TreeNode* n_77 = new TreeNode(-77);
+	TreeNode* n_40 = new TreeNode(40);
+	n->left = n_35;
+	n->right = n_77;
+	n_77->right = n_40;
+	vec = largestValues(n);
+	for (int t : vec)cout << t << endl;
+	//test end
+
+	system("pause");
+	return 0;
+}
---
+++ 20180908Leetcode刷题/894_所有可能的满二叉树_中等/894_所有可能的满二叉树_中等.cpp
@@ -0,0 +1,79 @@
+﻿#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include"math.h"
+#include<list>
+using namespace std;
+
+//满二叉树是一类二叉树，其中每个结点恰好有 0 或 2 个子结点。
+//
+//返回包含 N 个结点的所有可能满二叉树的列表。 答案的每个元素都是一个可能树的根结点。
+//
+//答案中每个树的每个结点都必须有 node.val=0。
+//
+//你可以按任何顺序返回树的最终列表。
+//
+//示例：
+//输入：7
+//输出：[[0,0,0,null,null,0,0,null,null,0,0],[0,0,0,null,null,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,null,null,null,null,0,0],[0,0,0,0,0,null,null,0,0]]
+//解释：
+////图链接https://leetcode-cn.com/problems/all-possible-full-binary-trees/description/
+//
+//提示：
+//1 <= N <= 20
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+vector<TreeNode*> allPossibleFBT(int N) {
+
+}
+
+void Qianxubianli(TreeNode* pNode)
+{
+	if (pNode == NULL)
+		return;
+
+	cout << pNode->val << " ,";
+	Qianxubianli(pNode->left);
+	Qianxubianli(pNode->right);
+}
+
+void Zhongxubianli(TreeNode* pNode)
+{
+	if (pNode == NULL)
+		return;
+
+	Zhongxubianli(pNode->left);
+	cout << pNode->val << " ,";
+	Zhongxubianli(pNode->right);
+}
+
+void Houxubianli(TreeNode* pNode)
+{
+	if (pNode == NULL)
+		return;
+
+	Houxubianli(pNode->left);
+	Houxubianli(pNode->right);
+	cout << pNode->val << " ,";
+}
+
+int main() {
+	//test1
+	vector<TreeNode*>res = allPossibleFBT(7);
+	for (int i = 0; i < res.size(); ++i){
+		cout << "test"<<i << endl;
+		Qianxubianli(res[i]);
+		cout << endl;
+	}
+	//test end
+
+	system("pause");
+	return 0;
+}
