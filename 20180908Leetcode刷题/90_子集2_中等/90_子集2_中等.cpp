97e49d5cf0451b4cba1719fa20f393633cbcf0f2
ty2 <1756625653@qq.com>
2018-11-02 14:16:44 +0800

    update

---
+++ 20180908Leetcode刷题/90_子集2_中等/90_子集2_中等.cpp
@@ -0,0 +1,43 @@
+﻿#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include<stack>
+#include<map>
+#include<unordered_map>
+#include<set>
+using namespace std;
+
+//给定一个可能包含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+//
+//说明：解集不能包含重复的子集。
+//
+//示例:
+//输入: [1,2,2]
+//输出:
+//[
+//  [2],
+//  [1],
+//  [1,2,2],
+//  [2,2],
+//  [1,2],
+//  []
+//]
+
+vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+
+}
+
+int main() {
+	//test1
+	vector<int>nums = { 1, 2, 2 };
+	vector<vector<int>>res = subsetsWithDup(nums);
+	for (vector<int>vec : res){
+		for (int i : vec)
+			cout << i << ",";
+		cout << endl;
+	}
+	//test end
+	system("pause");
+	return 0;
+}
