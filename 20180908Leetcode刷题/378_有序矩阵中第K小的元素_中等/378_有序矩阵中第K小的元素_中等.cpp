4631e1f35d508e65f134f4173c5a63fbcebca1b4
ty2 <1756625653@qq.com>
2018-11-02 16:37:47 +0800

    update

--- 20180908Leetcode刷题/378_有序矩阵中第K小的元素_中等/378_有序矩阵中第K小的元素_中等.cpp
+++ 20180908Leetcode刷题/378_有序矩阵中第K小的元素_中等/378_有序矩阵中第K小的元素_中等.cpp
@@ -3,6 +3,7 @@
 #include<iostream>
 #include<algorithm>
 #include<queue>
+#include<functional>
 using namespace std;
 
 //给定一个 n x n 矩阵，其中每行和每列元素均按升序排序，找到矩阵中第k小的元素。
@@ -21,9 +22,61 @@ using namespace std;
 //说明: 
 //你可以假设 k 的值永远是有效的, 1 ≤ k ≤ n2 。
 
-//此题使用二分查找，需注意其中索引的变化策略
-int kthSmallest(vector<vector<int>>& matrix, int k) {
-	priority_queue<int, vector<int>, less<int>>q;
+//初解
+//int kthSmallest(vector<vector<int>>& matrix, int k) {
+//	priority_queue<int, vector<int>, greater<int>>q;//最小堆
+//	for (vector<int>vec : matrix)
+//		for (int i : vec)
+//			q.push(i);
+//	while (k>1){
+//		q.pop();
+//		k--;
+//	}
+//	return q.top();
+//}
+
+//使用最大堆的方法，priority_queue默认是最大堆
+//int kthSmallest(vector<vector<int>>& matrix, int k){
+//	priority_queue<int>q;
+//	for (vector<int>vec : matrix){
+//		for (int m : vec){
+//			q.push(m);
+//			if (q.size() > k)q.pop();
+//		}
+//	}
+//	return q.top();
+//}
+
+//https://www.cnblogs.com/grandyang/p/5727892.html
+//函数功能:从左下角开始遍历小于等于target的数目
+int search_less_equal(vector<vector<int>>& matrix, int target){
+	int n = matrix.size(), i = n - 1, j = 0, res = 0;
+	while (i >= 0 && j < n){
+		if (matrix[i][j] <= target){
+			res += i + 1;
+			++j;
+		}
+		else
+			--i;
+	}
+	return res;
+}
+//使用二分法求解，mid值取matrix的第一个值(最小值)和最后一个值(最大值)的平均数
+//统计mid在matrix中的排名
+//统计方法1:轮询每行，统计比它小的数的个数
+//统计方法2:利用matrix的潜在排序特性，从左下角开始
+//方法1时间复杂度O(nlogn*logK)(K为matrix最大最小值之差)(每次查找每行时采用二分查找，查找一次O(nlogn))
+//方法2时间复杂度O(nlogK)(查找一次O(n))
+int kthSmallest(vector<vector<int>>& matrix, int k){
+	int left = matrix[0][0], right = matrix.back().back();
+	while (left < right){
+		//int mid = (left + right) / 2;//容易超过int所能表示的最大范围，即容易溢出
+		int mid = left + (right - left) / 2;
+		int cnt = search_less_equal(matrix, mid);
+		if (cnt < k)left = mid + 1;
+		else right = mid;
+	}
+	return left;
 }
 
 int main() {
