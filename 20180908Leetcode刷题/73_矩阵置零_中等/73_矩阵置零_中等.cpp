76e99181de0c61d3b8164d36241ad29a4fd0e37f
ty2 <1756625653@qq.com>
2018-11-02 14:08:42 +0800

    update

--- 20180908Leetcode刷题/73_矩阵置零_中等/73_矩阵置零_中等.cpp
+++ 20180908Leetcode刷题/73_矩阵置零_中等/73_矩阵置零_中等.cpp
@@ -43,8 +43,60 @@ using namespace std;
 //一个简单的改进方案是使用 O(m + n) 的额外空间，但这仍然不是最好的解决方案。
 //你能想出一个常数空间的解决方案吗？
 
+//O(mn)空间是记录下所有0所在的位置坐标
+//O(m+n)空间记录下所有0所在的行号和列号
+//使用常数空间代表不能另行记录其他信息，必须边变化矩阵中的数字同时在数字当中隐藏记录所在信息
+//思路：在首行首列记录是否需要置0
 void setZeroes(vector<vector<int>>& matrix) {
-
+	bool row = false, col = false;
+	for (int i = 0; i < matrix[0].size(); ++i)
+	{
+		if (matrix[0][i] == 0){
+			row = true;
+			break;
+		}
+	}
+	for (int i = 0; i < matrix.size(); ++i)
+	{
+		if (matrix[i][0] == 0){
+			col = true;
+			break;
+		}
+	}
+	for (int i = 1; i < matrix.size(); ++i){
+		for (int j = 1; j < matrix[0].size(); ++j){
+			if (matrix[i][j] == 0){
+				matrix[0][j] = 0;
+				matrix[i][0] = 0;
+			}
+		}
+	}
+	//开始进行首行首列以外的置零操作
+	for (int i = 1; i < matrix[0].size(); ++i)//每列置0
+	{
+		if (matrix[0][i] == 0){
+			for (int j = 1; j < matrix.size(); ++j)
+				matrix[j][i] = 0;
+		}
+	}
+	for (int i = 1; i < matrix.size(); ++i){
+		if (matrix[i][0] == 0){
+			for (int j = 1; j < matrix[0].size(); ++j){
+				matrix[i][j] = 0;
+			}
+		}
+	}
+	if (row){
+		for (int i = 0; i < matrix[0].size(); ++i)
+		{
+			matrix[0][i] = 0;
+		}
+	}
+	if (col){
+		for (int i = 0; i < matrix.size(); ++i){
+			matrix[i][0] = 0;
+		}
+	}
 }
 
 int main() {
@@ -64,6 +116,16 @@ int main() {
 	matrix = { { 0, 1, 2, 0 },
 	{ 3, 4, 5, 2 },
 	{ 1, 3, 1, 5 } };
+	setZeroes(matrix);
+	for (vector<int>vec : matrix)
+	{
+		for (int i : vec)
+			cout << i << ",";
+		cout << endl;
+	}
+	
+	//test3
+	matrix = { { 5 }, { 2 }, { 0 }, { 3 }, { 6 }, { 7 }, { 2 } };
 	setZeroes(matrix);
 	for (vector<int>vec : matrix)
 	{
