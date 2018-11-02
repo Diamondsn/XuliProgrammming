d8ca654472e0821ed234fa341d9a4d47afe75ad5
ty2 <1756625653@qq.com>
2018-11-02 14:31:48 +0800

    update'

--- 20180908Leetcode刷题/90_子集2_中等/90_子集2_中等.cpp
+++ 20180908Leetcode刷题/90_子集2_中等/90_子集2_中等.cpp
@@ -24,14 +24,42 @@ using namespace std;
 //  []
 //]
 
-vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+void dfs(set<vector<int>>& myset, vector<int>&vec, vector<int>&nums, int target){
+	if (target == nums.size())
+		myset.insert(vec);
+	else
+	{
+		vec.push_back(nums[target]);
+		dfs(myset, vec, nums, target + 1);
+		vec.pop_back();
+		dfs(myset, vec, nums, target + 1);
+	}
+}
 
+vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+	vector<vector<int>>res;
+	set<vector<int>>myset;
+	vector<int>vec;
+	sort(nums.begin(), nums.end());
+	dfs(myset, vec, nums, 0);
+	res.assign(myset.begin(), myset.end());
+	return res;
 }
 
 int main() {
 	//test1
 	vector<int>nums = { 1, 2, 2 };
 	vector<vector<int>>res = subsetsWithDup(nums);
+	for (vector<int>vec : res){
+		for (int i : vec)
+			cout << i << ",";
+		cout << endl;
+	}
+
+	//test2
+	nums = { 4, 4, 4, 1, 4 };
+	res = subsetsWithDup(nums);
+	cout << "test2" << endl;
 	for (vector<int>vec : res){
 		for (int i : vec)
 			cout << i << ",";
