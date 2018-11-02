9959d0b41ab305df03eda657ed2f8e087ccb8241
ty2 <1756625653@qq.com>
2018-11-02 15:15:31 +0800

    update'

--- 20180908Leetcode刷题/173_二叉搜索树迭代器_中等/173_二叉搜索树迭代器_中等.cpp
+++ 20180908Leetcode刷题/173_二叉搜索树迭代器_中等/173_二叉搜索树迭代器_中等.cpp
@@ -22,21 +22,34 @@ struct TreeNode {
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+//初解即想到标准解法，不断push左节点
 class BSTIterator {
 public:
 	BSTIterator(TreeNode *root) {
-
+		while (root)
+		{
+			mystack.push(root);
+			root = root->left;
+		}
 	}
 
 	/** @return whether we have a next smallest number */
 	bool hasNext() {
-
+		return !mystack.empty();
 	}
 
 	/** @return the next smallest number */
 	int next() {
-
+		TreeNode* pNode=mystack.top();
+		mystack.pop();
+		TreeNode* node = pNode->right;
+		while (node){
+			mystack.push(node);
+			node = node->left;
+		}
+		return pNode->val;
 	}
+	stack<TreeNode*>mystack;
 };
 
 /**
@@ -47,7 +60,21 @@ public:
 
 int main() {
 	//test1
-	
+	TreeNode* root = new TreeNode(5);
+	TreeNode* r3 = new TreeNode(3);
+	TreeNode* r8 = new TreeNode(8);
+	TreeNode* r1 = new TreeNode(1);
+	TreeNode* r4 = new TreeNode(4);
+	TreeNode* r6 = new TreeNode(6);
+	TreeNode* r9 = new TreeNode(9);
+	root->left = r3;
+	root->right = r8;
+	r3->left = r1;
+	r3->right = r4;
+	r8->left = r6;
+	r8->right = r9;
+	BSTIterator it = BSTIterator(root);
+	while (it.hasNext())cout << it.next() << endl;//输出结果为1,3,4,5,6,8,9,
 	//test end
 	system("pause");
 	return 0;
---
+++ 20180908Leetcode刷题/378_有序矩阵中第K小的元素_中等/378_有序矩阵中第K小的元素_中等.cpp
@@ -0,0 +1,35 @@
+﻿#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+//给定一个 n x n 矩阵，其中每行和每列元素均按升序排序，找到矩阵中第k小的元素。
+//请注意，它是排序后的第k小元素，而不是第k个元素。
+//
+//示例:
+//matrix = [
+//   [ 1,  5,  9],
+//   [10, 11, 13],
+//   [12, 13, 15]
+//],
+//k = 8,
+//
+//返回 13。
+//
+//说明: 
+//你可以假设 k 的值永远是有效的, 1 ≤ k ≤ n2 。
+
+int kthSmallest(vector<vector<int>>& matrix, int k) {
+
+}
+
+int main() {
+	//test1
+	vector<vector<int>>matrix = { {1,5,9}, {10,11,13}, {12,13,15} };
+	int a = kthSmallest(matrix, 8);
+	cout << a << endl;
+	//test end
+	system("pause");
+	return 0;
+}
