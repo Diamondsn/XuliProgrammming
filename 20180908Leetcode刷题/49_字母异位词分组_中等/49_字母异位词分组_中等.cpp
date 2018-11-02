9b5fbb310a284a29e19c519183dc2c1e048b2a17
ty2 <1756625653@qq.com>
2018-11-02 11:12:24 +0800

    update

--- 20180908Leetcode刷题/49_字母异位词分组_中等/49_字母异位词分组_中等.cpp
+++ 20180908Leetcode刷题/49_字母异位词分组_中等/49_字母异位词分组_中等.cpp
@@ -3,6 +3,9 @@
 #include<iostream>
 #include<algorithm>
 #include<stack>
+#include<map>
+#include<unordered_map>
+#include<set>
 using namespace std;
 
 //给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
@@ -20,17 +23,52 @@ using namespace std;
 //所有输入均为小写字母。
 //不考虑答案输出的顺序。
 
+//初解为按计数分类
 vector<vector<string>> groupAnagrams(vector<string>& strs) {
-
+	vector<int>vec(26, 0);
+	vector<vector<string>>res;
+	map<vector<int>, vector<string>>hash;
+	for (string s : strs){
+		vec.assign(26,0);
+		for (char c : s){
+			vec[c-'a']++;
+		}
+		hash[vec].push_back(s);
+	}
+	for (auto it = hash.begin(); it != hash.end(); ++it)
+	{
+		res.push_back(it->second);
+	}
+	return res;
 }
+
+//也可按照排序后的string进行分类
+//vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//	vector<vector<string> > res;
+//	unordered_map<string, vector<string> > m;
+//	for (auto str : strs){
+//		string t = str;
+//		sort(t.begin(), t.end());
+//		m[t].push_back(str);
+//	}
+//
+//	for (auto a : m){
+//		res.push_back(a.second);
+//	}
+//
+//	return res;
+//}
 
 int main() {
 	//test1
 	vector<string>strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
 	vector<vector<string>>res = groupAnagrams(strs);
 	for (vector<string>& vec : res)
+	{
 		for (string s : vec)
-			cout << s << endl;
+			cout << s << ",";
+		cout << endl;
+	}
 	//test end
 	system("pause");
 	return 0;
