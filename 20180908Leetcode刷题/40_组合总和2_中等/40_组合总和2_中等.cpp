60a0b45d0a1ed9adbfbbb568c011307e3782b26f
ty2 <1756625653@qq.com>
2018-11-02 17:11:21 +0800

    update

--- 20180908Leetcode刷题/40_组合总和2_中等/40_组合总和2_中等.cpp
+++ 20180908Leetcode刷题/40_组合总和2_中等/40_组合总和2_中等.cpp
@@ -3,7 +3,7 @@
 #include<iostream>
 #include<algorithm>
 #include<queue>
-#include<functional>
+#include<set>
 using namespace std;
 
 //给定一个数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
@@ -32,8 +32,56 @@ using namespace std;
 //  [5]
 //]
 
-vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+//初解:使用set代替去重策略，增加了时间和空间开销
+//void dfs(set<vector<int>>& myset, vector<int>&path, vector<int>&candidates, int target, int suoyin){
+//	if (target < 0||suoyin>candidates.size())return;
+//	
+//	if (target == 0){
+//		myset.insert(path);
+//	}
+//	else{
+//		for (int i = suoyin; i < candidates.size(); ++i){
+//			path.push_back(candidates[i]);
+//			dfs(myset, path, candidates, target - candidates[i], i + 1);
+//			path.pop_back();
+//		}
+//	}
+//}
+//
+//vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+//	vector<vector<int>>res;
+//	vector<int>path;
+//	set<vector<int>>myset;
+//	sort(candidates.begin(), candidates.end());
+//	dfs(myset, path, candidates, target, 0);
+//	res.assign(myset.begin(), myset.end());
+//	return res;
+//}
 
+void dfs(vector<vector<int>>& res, vector<int>&path, vector<int>&candidates, int target, int suoyin){
+	if (target < 0 || suoyin>candidates.size())return;
+
+	if (target == 0){
+		res.push_back(path);
+	}
+	else{
+		for (int i = suoyin; i < candidates.size(); ++i){
+			//以下去重，注意suoyin即使和之前的candidates[suoyin-1]相同也可添加
+			if (i != 0 && candidates[i] == candidates[i - 1]&&i>suoyin)continue;
+
+			path.push_back(candidates[i]);
+			dfs(res, path, candidates, target - candidates[i], i + 1);
+			path.pop_back();
+		}
+	}
+}
+
+vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+	vector<vector<int>>res;
+	vector<int>path;
+	sort(candidates.begin(), candidates.end());
+	dfs(res, path, candidates, target, 0);
+	return res;
 }
 
 int main() {
@@ -49,6 +97,7 @@ int main() {
 
 	//test2
 	cout << "test2" << endl;
+	candidates = { 2, 5, 2, 1, 2 };
 	res = combinationSum2(candidates, 5);
 	for (vector<int>vec : res){
 		for (int m : vec)
