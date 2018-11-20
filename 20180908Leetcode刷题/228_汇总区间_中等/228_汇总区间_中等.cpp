4ea1aed52b0062f8b60e32bac94078f3c12e1e20
ty2 <1756625653@qq.com>
2018-11-20 15:49:21 +0800

    update

--- 20180908Leetcode刷题/228_汇总区间_中等/228_汇总区间_中等.cpp
+++ 20180908Leetcode刷题/228_汇总区间_中等/228_汇总区间_中等.cpp
@@ -20,12 +20,48 @@ using namespace std;
 //解释: 2,3,4 可组成一个连续的区间; 8,9 可组成一个连续的区间。
 
 vector<string> summaryRanges(vector<int>& nums) {
-
+	if (nums.size()<=0)return{};
+	int before = nums[0],after=nums[0];
+	vector<string>res;
+	for (int i = 1; i < nums.size(); ++i){
+		if (nums[i] == after + 1){
+			after = nums[i];
+		}
+		else{
+			if (after == before){
+				res.push_back(to_string(after));
+			}
+			else{
+				res.push_back(to_string(before)+"->"+to_string(after));
+			}
+			before = nums[i];
+			after = nums[i];
+		}
+	}
+	if (after == before){
+		res.push_back(to_string(after));
+	}
+	else{
+		res.push_back(to_string(before) + "->" + to_string(after));
+	}
+	return res;
 }
 
 int main() {
 	//test1
-	
+	vector<int>nums = { 0, 1, 2, 4, 5, 7 };
+	vector<string>res = summaryRanges(nums);
+	for (string str : res){
+		cout << str << endl;
+	}
+
+	//test2
+	cout << endl << "test2" << endl;
+	nums = { 0, 2, 3, 4, 6, 8, 9 };
+	res = summaryRanges(nums);
+	for (string str : res){
+		cout << str << endl;
+	}
 	//test end
 
 	system("pause");
