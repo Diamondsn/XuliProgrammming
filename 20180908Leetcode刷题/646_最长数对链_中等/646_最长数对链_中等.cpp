bf76707cd384b4847d8ad044ab6c1d87d5c72ad1
ty2 <1756625653@qq.com>
2018-11-20 14:42:17 +0800

    update

--- 20180908Leetcode刷题/646_最长数对链_中等/646_最长数对链_中等.cpp
+++ 20180908Leetcode刷题/646_最长数对链_中等/646_最长数对链_中等.cpp
@@ -21,20 +21,58 @@ using namespace std;
 //注意：
 //给出数对的个数在 [1, 1000] 范围内。
 
-bool bijiao(vector<int>& a, vector<int>& b){
-	return a.front() < b.front();
+//初次尝试，排序的依据都不正确，要根据数对中的第二个数进行排序
+//bool bijiao(vector<int>& a, vector<int>& b){
+//	return a.front() < b.front();
+//}
+//int findLongestChain(vector<vector<int>>& pairs) {
+//	sort(pairs.begin(), pairs.end(), bijiao);
+//	vector<int>dp(pairs.size());
+//	dp[0] = 1;
+//	int min = pairs[0][1];
+//	for (int i = 1; i < pairs.size(); ++i){
+//		if (pairs[i][0] > min){
+//			dp[i] = dp[i - 1] + 1;
+//		}
+//		else{
+//			dp[i] = dp[i - 1];
+//		}
+//	}
+//	return dp.back();
+//}
+
+//https://blog.csdn.net/qq_26410101/article/details/80876280
+//法一:贪心算法，将数对根据第二个数进行排序后，如果满足后一个数对的第一个数大于最后的end，则长度加一。
+//bool bijiao(vector<int>a,vector<int>b){
+//	return a[1] < b[1];
+//}
+
+//int findLongestChain(vector<vector<int>>& pairs) {
+//	sort(pairs.begin(), pairs.end(), bijiao);
+//	int res = 0,end=INT_MIN;
+//	for (vector<int>pair:pairs){
+//		if (pair[0] > end)
+//		{
+//			end = pair[1];
+//			res++;
+//		}
+//	}
+//	return res;
+//}
+
+//法二:动态规划，dp[i]是以第i个数对结尾的最长数对链的长度,返回dp中的最大值
+//此题变化非常大，解法非常多，可细致深入思考动规条件
+bool bijiao1(vector<int>a, vector<int>b){
+	return a[0] < b[0];
 }
 int findLongestChain(vector<vector<int>>& pairs) {
-	sort(pairs.begin(), pairs.end(), bijiao);
-	vector<int>dp(pairs.size());
-	dp[0] = 1;
-	int min = pairs[0][1];
+	sort(pairs.begin(), pairs.end(), bijiao1);
+	vector<int> dp(pairs.size(), 1);
 	for (int i = 1; i < pairs.size(); ++i){
-		if (pairs[i][0] > min){
-			dp[i] = dp[i - 1] + 1;
-		}
-		else{
-			dp[i] = dp[i - 1];
+		for (int j = 0; j < i; j++){
+			if (pairs[i][0] > pairs[j][1]){
+				dp[i] = std::max(dp[i], dp[j] + 1);
+			}
 		}
 	}
 	return dp.back();
