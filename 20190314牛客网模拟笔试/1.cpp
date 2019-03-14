5484693f1e13023f8394b318822a1b4f57c68a7f
xl <1756625653@qq.com>
2019-03-14 21:42:53 +0800

    update

---
+++ 20190314牛客网模拟笔试/1.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	int  n;
+	cin>>n;
+	string a,b;
+	cin>>a>>b;
+	int result=0;
+	for(int i=0;i<a.size();++i){
+       int temp= abs(a[i]-b[i]);
+	   int t=temp>10-temp?10-temp:temp;
+	   result+=t;
+}
+	cout<<result;
+	system("pause");
+}
