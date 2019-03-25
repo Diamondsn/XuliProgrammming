8d662f5ea06dc9edcc9669da680c2fffc0d2d250
ty2 <1756625653@qq.com>
2019-03-25 14:23:55 +0800

    update

--- 前端笔记/Program/文章中输出次数最多的单词.js
+++ 前端笔记/Program/文章中输出次数最多的单词.js
@@ -53,3 +53,40 @@ function counts(article){
    console.log(maxword+" "+max);
 }
 counts("Age has reached the end of the beginning of a word. May be guilty in his seems to passing a lot of different life became the appearance of the same day;");
+
+
+
+//另解
+function test1(str){
+    var arr=str.split(/[,.; ]/);
+    let obj={};
+    for(let i=0;i<arr.length;++i){
+         if(arr[i]!==' '||arr[i]!==','||arr[i]!=='.'||arr[i]!==''){
+             if(obj[arr[i]]){
+                 obj[arr[i]]++;
+             }else{
+                 obj[arr[i]]=1;
+             }
+         }
+    }
+    let max=0;
+    let res=[];
+    let count=[];
+    for(item in obj){
+        let key=item;
+        let value=obj[key];
+        if(value>max){
+            res.length=0;
+            count.length=0;
+            res.push(key);
+            count.push(value);
+        }else if(value==max){
+            res.push(key);
+            count.push(value);
+        }
+        max=value>max?value:max;
+    }
+    return{word:res,time:max};
+}
+
+console.log(test1("Age has reached the end of the beginning of a word. May be guilty in his seems to passing a lot of different life became the appearance of the same day;"));
