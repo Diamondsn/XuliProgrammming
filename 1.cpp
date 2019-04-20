e793d156407af1729870babf0c23472f714c94e8
Diamondsn <Diamondsn@users.noreply.github.com>
2019-04-20 21:28:56 +0800

    update

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+#include<string>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include<stack>
+#include<math.h>
+#include<map>
+#include<set>
+
+using namespace std;
+
+int main() {
+	system("pause");
+	return 0;
+}
+
+
