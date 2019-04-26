5b0dbd8a486d950970c503476535436ac1e92cb6
ty2 <1756625653@qq.com>
2019-04-26 09:50:24 +0800

    update

--- 前端笔记/Program/编程总结.js
+++ 前端笔记/Program/编程总结.js
@@ -13,21 +13,37 @@ function shuchu(node){
 }
 var node3=new Node(3,null);
 var node2=new Node(2,node3);
-var head=new Node(1,node1);
+var head=new Node(1,node2);
 shuchu(head);
 console.log("更改方向");
 shuchu(reverseList(head));
 //1解 递归
+// function reverseList(node) {
+//     if(!node)
+//     return node;
+//     if(!node.next)
+//     return node;
+//     var curhead=reverseList(node.next);
+//     var temphead=curhead;
+//     while(temphead && temphead.next){
+//         temphead=temphead.next;
+//     }
+//     temphead.next=node;
+//     node.next=null;
+//     return curhead;
+// }
+//2解 双指针
 function reverseList(node) {
-    if(!node)
-    return node;
-    if(!node.next)
-    return node;
-    var curhead=reverseList(node.next);
-    var temphead=curhead;
-    while(temphead && temphead.next){
-        temphead=temphead.next;
+    if(!node ||!node.next)
+     return node;
+     var node1=null;
+     var node2=node;
+    while(node2){
+        var temp=node2.next;
+        node2.next=node1;
+        node1=node2;
+        node2=temp;
     }
-    temphead.next=node;
-    return curhead;
+    return node1;
+
 }
